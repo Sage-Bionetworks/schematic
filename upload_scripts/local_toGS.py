a57bd8f854e9d442baf919015ca6a48379f2e9e5
xdoan <xengiedoan@gmail.com>
2019-06-10 12:02:12 -0700

    updated for parallel threaded upload

--- upload_scripts/local_toGS.py
+++ upload_scripts/local_toGS.py
@@ -14,4 +14,9 @@ import os
 
 filepath = '/Users/xdoan/Shell/HTAN-data-pipeline/test_data/test.txt'
 
-os.system("gsutil cp " + filepath + " gs://htan-data-model-testing "  )
+# os.system("gsutil cp " + filepath + " gs://htan-data-model-testing "  )
+
+### for larger files
+### -m for multithreaded 
+### parallel composite upload for files > 150MB
+os.system("gsutil -o GSUtil:parallel_composite_upload_threshold=150M -m cp " + filepath + " gs://htan-data-model-testing "  )
