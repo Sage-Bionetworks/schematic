c045ecd5b00d2518db53088b21d156567eed4c86
Milen Nikolov <milen.nikolov@sagebase.org>
2019-06-20 14:44:37 -0700

    Added validation funciton with validation success/error handling

--- metadata_model_driver.py
+++ metadata_model_driver.py
@@ -9,3 +9,9 @@ mm = MetadataModel(inputMModelLocation, inputMModelLocationType)
 manifest_url = mm.getModelManifest(datasetType)
 
 print(manifest_url)
+
+manifest_path = "./manifest.csv"
+
+annotation_status = mm.validateModelManifest(manifest_path, datasetType)
+
+print(annotation_status)
