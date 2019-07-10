978dbd635494c291b522f94ffd2a4de05d6af50a
Milen Nikolov <milen.nikolov@sagebase.org>
2019-07-09 20:11:02 -0700

    Added a function to populate manifest from existing one, returning the populated manifest google sheet url; added corresponding test

--- metadata_model_driver.py
+++ metadata_model_driver.py
@@ -5,6 +5,11 @@ inputMModelLocation = "./schemas/scRNASeq.jsonld"
 inputMModelLocationType = "local"
 datasetType = "scRNASeq"
 
+
+# testing metadata model based manifest generation
+print("*****************************************************")
+print("Testing metamodel-based manifest generation")
+print("*****************************************************")
 mm = MetadataModel(inputMModelLocation, inputMModelLocationType)
 
 manifest_url = mm.getModelManifest(datasetType, filenames = ["1.txt", "2.txt", "3.txt"])
@@ -12,8 +17,21 @@ manifest_url = mm.getModelManifest(datasetType, filenames = ["1.txt", "2.txt", "
 print(manifest_url)
 
 
+print("*****************************************************")
+print("Testing metamodel-based validation")
+print("*****************************************************")
 manifest_path = "./manifest.csv"
 
 annotation_errors = mm.validateModelManifest(manifest_path, datasetType)
 
 print(annotation_errors)
+
+
+print("*****************************************************")
+print("Testing metamodel-based manifest population")
+print("*****************************************************")
+
+# get a sheet prepopulated with an existing manifest; returns a url to a google sheet
+prepopulated_manifest_url = mm.populateModelManifest(manifest_path, datasetType)
+print(prepopulated_manifest_url)
+
