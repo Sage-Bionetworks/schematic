37a8e4e1738f3f774aea72dd1e6163a37f0cb427
milen-sage <41303818+milen-sage@users.noreply.github.com>
2019-09-25 17:55:16 -0700

    Revert "Syncing up fork with upstream"

--- metadata_model_test_driver.py
+++ metadata_model_test_driver.py
@@ -1,11 +1,13 @@
 from MetadataModel import MetadataModel
 
 #inputMModelLocation = "./schemas/exampleSchemaReq.jsonld"
-inputMModelLocation = "./schemas/scRNASeq.jsonld"
+#inputMModelLocation = "./schemas/scRNASeq.jsonld"
+inputMModelLocation = "./schemas/HTAPP.jsonld"
 #inputMModelLocation = "./data/NFSchemaReq.jsonld"
 inputMModelLocationType = "local"
-datasetType = "scRNASeq"
+#datasetType = "scRNASeq"
 #datasetType = "Thing"
+datasetType = "HTAPP"
 
 
 
