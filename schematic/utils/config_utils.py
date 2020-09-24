8da1230e3d08d62351786e6fc8ade46c73bb9a67
Bruno Grande <bruno.grande@sagebase.org>
2020-09-24 00:24:01 -0700

    Update schematic to use new CONFIG object

--- schematic/utils/config_utils.py
+++ schematic/utils/config_utils.py
@@ -1,11 +1,3 @@
-import yaml
+from ..configuration import Configuration
 
-def load_yaml(file_path: str) -> dict:
-    with open(file_path, 'r') as stream:
-        try:
-            config_data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return None
-
-    return config_data
+load_yaml = Configuration.load_yaml
