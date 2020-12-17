b1fcf9c832901c41d5478e87164f22840af7d65f
Sujay Patil <sujaysanjeev.patil@gmail.com>
2020-12-17 12:01:32 -0800

    modifed get_manifest() CLI to accommodate fill_in_from_config() changes

--- schematic/manifest/commands.py
+++ schematic/manifest/commands.py
@@ -2,6 +2,7 @@
 
 import warnings
 import click
+
 from schematic.manifest.generator import ManifestGenerator
 from schematic.utils.cli_utils import fill_in_from_config
 from schematic import CONFIG
@@ -27,8 +28,8 @@ def manifest(): # use as `schematic manifest ...`
 @click.option('-d', '--dataset_id', help='SynID of existing dataset on Synapse.')
 @click.option('-s', '--sheet_url', type=bool, help='Enable/disable URL generation.')
 @click.option('-j', '--json_schema', help='Path to JSON Schema (validation schema).')
-@click.option('-c', '--config', help='Path to schematic configuration file.')
-def get_manifest(title, data_type, path_to_json_ld, 
+@click.option('-c', '--config', help='Path to schematic configuration file.', required=True)
+def get_manifest(title, data_type, jsonld, 
                  dataset_id, sheet_url, json_schema, 
                  config):
     """
@@ -39,21 +40,20 @@ def get_manifest(title, data_type, path_to_json_ld,
     # optional parameters that need to be passed to ManifestGenerator()
     # can be read from config.yml as well
     title = fill_in_from_config(
-        "title", title, CONFIG, ("manifest", "title")
+        "title", title, ("manifest", "title")
     )
     data_type = fill_in_from_config(
-        "data_type", data_type, CONFIG, ("manifest", "data_type")
+        "data_type", data_type, ("manifest", "data_type")
     )
-    path_to_json_ld = fill_in_from_config(
-        "path_to_json_ld", path_to_json_ld, CONFIG, ("model", "input", "location")
+    jsonld = fill_in_from_config(
+        "jsonld", jsonld, ("model", "input", "location")
     )
     json_schema = fill_in_from_config(
-        "json_schema", json_schema, CONFIG, ("model", "input", "validation_schema")
+        "json_schema", json_schema, ("model", "input", "validation_schema")
     )
-
     # create object of type ManifestGenerator
     manifest_generator = ManifestGenerator(title=title,
-                                           path_to_json_ld=path_to_json_ld,
+                                           path_to_json_ld=jsonld,
                                            root=data_type)
         
     # call get_manifest() on manifest_generator
