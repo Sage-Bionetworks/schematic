6974960c3e4a7768ad498170d87a0d51643b6e49
Bruno Grande <bruno.grande@sagebase.org>
2021-01-28 23:31:54 -0800

    Add basic test config with minimal fields

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,18 @@
-
+import os
 import logging
 import pytest
 
+from schematic.configuration import CONFIG
+
+
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(TESTS_DIR, "data")
+CONFIG_PATH = os.path.join(DATA_DIR, "test_config.yml")
+CONFIG.load_config(CONFIG_PATH)
 
 
 @pytest.fixture()
