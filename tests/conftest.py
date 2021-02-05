3adc4beda67768c4bfe2e9b0856c6a7fa25f093f
Bruno Grande <bruno.grande@sagebase.org>
2021-02-05 11:27:51 -0800

    Silence verbose loggers for debugging

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,6 +9,12 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
+# Silence some very verbose loggers
+logging.getLogger("urllib3").setLevel(logging.INFO)
+logging.getLogger("googleapiclient").setLevel(logging.INFO)
+logging.getLogger("google_auth_httplib2").setLevel(logging.INFO)
+
+
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(TESTS_DIR, "data")
 CONFIG_PATH = os.path.join(DATA_DIR, "test_config.yml")
