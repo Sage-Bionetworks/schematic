e8d100b3deb6e0095c8d315a973cdafe2d1c7200
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-03-03 11:09:06 -0800

    Revert "suppress INFO warnings from rdflib as well"
    This reverts commit 9ac24d2c4ee1ad883b045be30eb0033e7d230cd6.

--- schematic/__init__.py
+++ schematic/__init__.py
@@ -10,6 +10,5 @@ logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
 
 # Suppress INFO-level logging from some dependencies
 logging.getLogger('keyring').setLevel(logging.ERROR)
-logging.getLogger('rdflib').setLevel(logging.ERROR)
 
 logger = logging.getLogger('schematic')
