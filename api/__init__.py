b6375a074b8f65366cab0ca6dd40b8933150f3a6
Jason Hwee <1216418+hweej@users.noreply.github.com>
2021-06-08 18:49:15 -0400

    Add some comments and examples

--- api/__init__.py
+++ api/__init__.py
@@ -6,4 +6,23 @@ def create_app():
     connexionapp.add_api("api.yaml")
     app = connexionapp.app
 
+    # Configure schematic & do the error handling
+    # Call schematic.load_config()
+    # keys = schematic get_config()
+
+    # Configure flask app
+    # app.config[] = schematic[]
+    # app.config[] = schematic[]
+    # app.config[] = schematic[]
+
+    # Initialize extension schematic
+    # import MyExtension
+    # myext = MyExtension()
+    # myext.init_app(app)
+
     return app
+
+# def route_code():
+#     import flask_schematic as sc
+#     sc.method1()
+# 
--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
 import os
 
 import connexion
+from flask import current_app, request, g
 
 from schematic import CONFIG
 
@@ -8,11 +9,23 @@ from schematic.utils.cli_utils import get_from_config
 from schematic.manifest.generator import ManifestGenerator
 
 
+# def before_request(var1, var2):
+#     # Do stuff before your route executes
+#     pass
+# def after_request(var1, var2):
+#     # Do stuff after your route executes
+#     pass
+
+# @before_request
 def get_manifest_route(title, oauth, use_annotations):
     CONFIG.load_config("./config.yml")
-
     jsonld = get_from_config(CONFIG.DATA, ("model", "input", "location"))
 
+    # current_app.config['model']
+    # current_app.config['input']
+    # current_app.config['location']
+
+    # request.data[]
     data_type = connexion.request.args["data_type"]
 
     # create object of type ManifestGenerator
