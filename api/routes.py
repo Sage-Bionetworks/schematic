75678b93c861707539e64367eb1c0bd6a68d09ad
Sujay Patil <sujaysanjeev.patil@gmail.com>
2021-06-25 11:58:01 -0700

    rudimentary endpoint for manifest validation

--- api/routes.py
+++ api/routes.py
@@ -1,14 +1,16 @@
 import os
 import shutil
 import tempfile
+import shutil
 import urllib.request
 
 import connexion
-from flask import current_app as app, request, g
+from flask import current_app as app, request, g, jsonify
 
 from schematic import CONFIG
 
 from schematic.manifest.generator import ManifestGenerator
+from schematic.models.metadata import MetadataModel
 
 
 # def before_request(var1, var2):
@@ -56,3 +58,42 @@ def get_manifest_route(schema_url, title, oauth, use_annotations):
     result = manifest_generator.get_manifest(sheet_url=True, dataset_id=dataset_id)
 
     return result
+
+
+def validate_manifest_route(schema_url, data_type):
+    manifest_file = connexion.request.files["csv_file"]
+
+    # save contents of incoming manifest CSV file to temp file
+    temp_dir = tempfile.gettempdir()
+    # path to temp file where manifest file contents will be saved
+    temp_path = os.path.join(temp_dir, manifest_file.filename)
+    # save content
+    manifest_file.save(temp_path)
+
+    # check if file exists at the path created, i.e., app.config['SCHEMATIC_CONFIG']
+    path_to_config = app.config["SCHEMATIC_CONFIG"]
+
+    if os.path.isfile(path_to_config):
+        CONFIG.load_config(path_to_config)
+    else:
+        raise FileNotFoundError(
+            f"No configuration file was found at this path: {path_to_config}"
+        )
+
+    # retrieve a JSON-LD via URL and store it in a temporary location
+    with urllib.request.urlopen(schema_url) as response:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".jsonld") as tmp_file:
+            shutil.copyfileobj(response, tmp_file)
+
+    # get path to temporary JSON-LD file
+    jsonld = tmp_file.name
+
+    metadata_model = MetadataModel(
+        inputMModelLocation=jsonld, inputMModelLocationType="local"
+    )
+
+    errors = metadata_model.validateModelManifest(
+        manifestPath=temp_path, rootNode=data_type
+    )
+
+    return errors
