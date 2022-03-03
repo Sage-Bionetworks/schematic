0f5e1d1f1d58705793f7dd2029147f2a00e1310b
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2022-03-03 16:29:00 -0700

    Update test_validation.py

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -14,12 +14,10 @@ logger = logging.getLogger(__name__)
 class TestManifestValidation:
     def test_valid_manifest(self,helpers):
         manifestPath = helpers.get_data_path("mock_manifests/Valid_Test_Manifest.csv")
-        #manifestPath = Path(os.path.join(Path(os.getcwd()).parent,'tests/data/mock_manifests/valid_test_manifest.csv')).resolve()
         rootNode = 'MockComponent'
 
         metadataModel= MetadataModel(
             inputMModelLocation =   helpers.get_data_path("example.model.jsonld"),
-            #inputMModelLocation = str(Path(os.path.join(os.getcwd(),'tests/data/example.model.jsonld')).resolve()),
             inputMModelLocationType="local"
             )
 
@@ -36,13 +34,11 @@ class TestManifestValidation:
 
     def test_invalid_manifest(self,helpers):
         manifestPath = helpers.get_data_path("mock_manifests/Invalid_Test_Manifest.csv")
-        #manifestPath = Path(os.path.join(Path(os.getcwd()).parent,'tests/data/mock_manifests/invalid_test_manifest.csv')).resolve()
         rootNode = 'MockComponent'
 
 
         metadataModel= MetadataModel(
             inputMModelLocation =   helpers.get_data_path("example.model.jsonld"),
-            #inputMModelLocation = str(Path(os.path.join(os.getcwd(),'tests/data/example.model.jsonld')).resolve()),
             inputMModelLocationType="local"
             )
 
