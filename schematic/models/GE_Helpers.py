8ddb9b580e4ff19dc7d85f33f551636ba50ddea7
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2022-03-25 09:46:15 -0700

    Revert "switched censoring behavior to only trigger on errors"
    This reverts commit b51b08b4eeb67e5dbbbc46993510ab33b7cc03c0.

--- schematic/models/GE_Helpers.py
+++ schematic/models/GE_Helpers.py
@@ -496,10 +496,13 @@ class GreatExpectationsHelpers(object):
                     if content_errors:
                         errors.append(content_errors)  
                         if rule.startswith('protectAges'):
-                            self.censor_ages(content_errors,errColumn)
+                            #self.censor_ages(content_errors,errColumn)
                             pass
                     elif content_warnings:
                         warnings.append(content_warnings)  
+                        if rule.startswith('protectAges'):
+                            #self.censor_ages(content_warnings,errColumn)
+                            pass
 
         return errors, warnings, self.manifest
 
@@ -542,7 +545,9 @@ class GreatExpectationsHelpers(object):
         """
         
         censor_rows = list(np.array(message[0]) - 2) 
-        self.manifest.loc[censor_rows,(col)] = 'age censored'
+        if self.manifest[col].dtype == 'int64':
+            self.manifest[col] = self.manifest[col].astype('Int64')
+        self.manifest.loc[censor_rows,(col)] = np.nan
 
         # update the manifest file, so that ages are censored
         self.manifest.to_csv(self.manifestPath, index=False)
