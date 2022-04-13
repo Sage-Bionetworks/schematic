c99477eb9ec25c36c8b8edd2a4368324d9b94ced
Mialy DeFelice <mialy.defelice@sagebase.org>
2022-04-13 16:09:54 -0700

    add annotations to manifest tables and CSVs

--- schematic/store/synapse.py
+++ schematic/store/synapse.py
@@ -489,8 +489,7 @@ class SynapseStorage(BaseStorage):
         # Rename the manifest columns to display names to match fileview
         blacklist_chars = ['(', ')', '.', ' ']
         manifest_columns = manifest.columns.tolist()
-        manifest_columns.remove('entityId')
-        
+
         cols = [
             se.get_class_label_from_display_name(
                 str(col)
@@ -498,10 +497,15 @@ class SynapseStorage(BaseStorage):
             for col in manifest_columns
         ]
 
-        cols.append('entityId')
+        cols = list(map(lambda x: x.replace('EntityId', 'entityId'), cols))
+
 
         # Reset column names in manifest
         manifest.columns = cols
+
+        #move entity id to end of df
+        entity_col = manifest.pop('entityId')
+        manifest.insert(len(manifest.columns), 'entityId', entity_col)
 
         # Get the column schema
         col_schema = as_table_columns(manifest)
@@ -532,7 +536,7 @@ class SynapseStorage(BaseStorage):
     
         return manifest_synapse_file_id
 
-    def format_annotations(self, se, row, entityId, useSchemaLabel, hideBlanks):
+    def format_row_annotations(self, se, row, entityId, useSchemaLabel, hideBlanks):
         # prepare metadata for Synapse storage (resolve display name into a name that Synapse annotations support (e.g no spaces, parenthesis)
         # note: the removal of special characters, will apply only to annotation keys; we are not altering the manifest
         # this could create a divergence between manifest column and annotations. this should be ok for most use cases.
@@ -575,6 +579,45 @@ class SynapseStorage(BaseStorage):
                     annos[anno_k] = ""
             else:
                 annos[anno_k] = anno_v
+        return annos
+
+    def format_manifest_annotations(self, manifest, manifest_synapse_id):
+        '''
+        Set annotations for the manifest (as a whole) so they can be applied to the manifest table or csv.
+        For now just getting the Component.
+        '''
+        
+        entity = self.syn.get(manifest_synapse_id, downloadFile=False)
+        is_file = entity.concreteType.endswith(".FileEntity")
+        is_table = entity.concreteType.endswith(".TableEntity")
+
+        if is_file:
+            # Get file metadata
+            metadata = self.getFileAnnotations(manifest_synapse_id)
+
+            # Gather component information
+            component = manifest['Component'].unique()
+            
+            # Double check that only a single component is listed, else raise an error.
+            try:
+                len(component) == 1
+            except ValueError as err:
+                raise ValueError(
+                    f"Manifest has more than one component. Please check manifest and resubmit."
+                ) from err
+
+            # Add component to metadata
+            metadata['Component'] = component[0]
+        elif is_table:
+            metadata = self.getTableAnnotations(manifest_synapse_id)
+        
+        # Get annotations
+        annos = self.syn.get_annotations(manifest_synapse_id)
+
+        # Add metadata to the annotations
+        for annos_k, annos_v in metadata.items():
+            annos[annos_k] = annos_v
+
         return annos
 
     def associateMetadataWithFiles(
@@ -672,14 +715,22 @@ class SynapseStorage(BaseStorage):
                 # get the entity id corresponding to this row
                 entityId = row["entityId"]
 
+            # Adding annotations to connected files.
             if entityId:
                 # Format annotations for Synapse
-                annos = self.format_annotations(se, row, entityId, useSchemaLabel, hideBlanks)
-                # Store annotations for an entity
+                annos = self.format_row_annotations(se, row, entityId, useSchemaLabel, hideBlanks)
+
+                # Store annotations for an entity folder
                 self.syn.set_annotations(annos)
-        
+
         # Load manifest to synapse as a CSV File
         manifest_synapse_file_id = self.uplodad_manifest_file(manifest, metadataManifestPath, datasetId)
+        
+        # Get annotations for the file manifest.
+        manifest_annotations = self.format_manifest_annotations(manifest, manifest_synapse_file_id)
+        
+        self.syn.set_annotations(manifest_annotations)
+
         logger.info("Associated manifest file with dataset on Synapse.")
             
         if manifest_record_type == 'table' or manifest_record_type == 'both':
@@ -692,8 +743,42 @@ class SynapseStorage(BaseStorage):
                 update_col = 'Uuid',
                 specify_schema = False,
                 )
+            
+            # Get annotations for the table manifest
+            manifest_annotations = self.format_manifest_annotations(manifest, manifest_synapse_table_id)
+            self.syn.set_annotations(manifest_annotations)
 
         return manifest_synapse_file_id
+
+    def getTableAnnotations(self, table_id:str):
+        """Generate dictionary of annotations for the given Synapse file.
+        Synapse returns all custom annotations as lists since they
+        can contain multiple values. In all cases, the values will
+        be converted into strings and concatenated with ", ".
+
+        Args:
+            fileId (str): Synapse ID for dataset file.
+
+        Returns:
+            dict: Annotations as comma-separated strings.
+        """
+        try:
+            entity = self.syn.get(table_id, downloadFile=False)
+            is_table = entity.concreteType.endswith(".TableEntity")
+            annotations_raw = entity.annotations
+        except SynapseHTTPError:
+            # If an error occurs with retrieving entity, skip it
+            # This could be caused by a temporary file view that
+            # was deleted since its ID was retrieved
+            is_file, is_table = False, False
+
+        # Skip anything that isn't a file or folder
+        if not (is_table):
+            return None
+
+        annotations = self.getEntityAnnotations(table_id, entity, annotations_raw)
+
+        return annotations
 
     def getFileAnnotations(self, fileId: str) -> Dict[str, str]:
         """Generate dictionary of annotations for the given Synapse file.
@@ -724,6 +809,11 @@ class SynapseStorage(BaseStorage):
         if not (is_file or is_folder):
             return None
 
+        annotations = self.getEntityAnnotations(fileId, entity, annotations_raw)
+
+        return annotations
+
+    def getEntityAnnotations(self, fileId, entity, annotations_raw):
         # Extract annotations from their lists and stringify. For example:
         # {'YearofBirth': [1980], 'author': ['bruno', 'milen', 'sujay']}
         annotations = dict()
