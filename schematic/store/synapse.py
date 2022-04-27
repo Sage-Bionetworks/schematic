d3477b26af0e616169ab846f901c02d3f03c6391
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2022-04-27 09:34:01 -0700

    restore default manifest record type

--- schematic/store/synapse.py
+++ schematic/store/synapse.py
@@ -633,7 +633,7 @@ class SynapseStorage(BaseStorage):
         return annos
 
     def associateMetadataWithFiles(
-        self, metadataManifestPath: str, datasetId: str, manifest_record_type: str, 
+        self, metadataManifestPath: str, datasetId: str, manifest_record_type: str = 'both', 
         useSchemaLabel: bool = True, hideBlanks: bool = False, restrict_manifest = False,
     ) -> str:
         """Associate metadata with files in a storage dataset already on Synapse.
