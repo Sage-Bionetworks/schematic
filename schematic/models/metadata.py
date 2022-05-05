de152d32e934e9e797f243fe512c35489eae6531
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2022-05-05 11:16:23 -0700

    type spec

--- schematic/models/metadata.py
+++ schematic/models/metadata.py
@@ -300,7 +300,7 @@ class MetadataModel(object):
         use_schema_label: bool = True,
         hide_blanks: bool = False,
         input_token: str = None,
-        project_scope = None,
+        project_scope: List = None,
     ) -> string:
         """Wrap methods that are responsible for validation of manifests for a given component, and association of the
         same manifest file with a specified dataset.
