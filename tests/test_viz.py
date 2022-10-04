0b98f2991f2d939a587752fd8cbd33bdc164c6b2
linglp <linglp@umich.edu>
2022-10-04 16:48:11 -0400

    update tests

--- tests/test_viz.py
+++ tests/test_viz.py
@@ -43,9 +43,7 @@ class TestVisualization:
         expect_col_names = ['Attribute', 'Label', 'Description',
                             'Required', 'Cond_Req', 'Valid Values', 'Conditional Requirements',
                             'Component']
-        expected_components = ['Biospecimen', 'Patient', 'BulkRNA-seqAssay',
-                               'ScRNA-seqLevel1', 'ScRNA-seqLevel2', 'ScRNA-seqLevel3',
-                               'ScRNA-seqLevel4', 'ImagingChannels', 'Imaging']
+        expected_components = ['Biospecimen', 'Patient', 'BulkRNA-seqAssay']
         
         # Get actual values
         actual_column_names = df.columns.tolist()
@@ -63,23 +61,22 @@ class TestVisualization:
         df= pd.read_csv(StringIO(text_str)).drop(columns=['Unnamed: 0'])
         
         # Define expected text associated with 'Patient' and 'Imaging' tree
-        expected_patient_text = ['Biospecimen', 'BulkRNA-seqAssay', 'ScRNA-seqLevel1', 'ScRNA-seqLevel2',
-                                 'ScRNA-seqLevel3', 'ScRNA-seqLevel4', 'ImagingChannels', 'Imaging']
+        expected_patient_text = ['Biospecimen', 'BulkRNA-seqAssay']
 
-        expected_imaging_text = ['Biospecimen', 'Patient', 'BulkRNA-seqAssay', 'ScRNA-seqLevel1',
-                                 'ScRNA-seqLevel2', 'ScRNA-seqLevel3', 'ScRNA-seqLevel4', 'ImagingChannels']
+        expected_Biospecimen_text = ['BulkRNA-seqAssay']
         
         # Get actual text
         actual_patient_text = df.loc[df['Component'] == 'Patient']['name'].tolist()
+        print('actual patient text', actual_patient_text)
 
-        actual_imaging_text = df.loc[df['Component'] == 'Imaging']['name'].tolist()
+        actual_Biospecimen_text = df.loc[df['Component'] == 'Biospecimen']['name'].tolist()
         
         # Check some random pieces of text we would assume to be in the plain text.
         assert ((df['Component'] == 'Patient') & (df['name'] == 'Biospecimen')).any()
         
         # Check the extracted text matches expected text.
         assert actual_patient_text == expected_patient_text
-        assert actual_imaging_text == expected_imaging_text
+        assert actual_Biospecimen_text == expected_Biospecimen_text
 
     def test_layers(self, helpers, tangled_tree):
         layers_str = tangled_tree.get_tangled_tree_layers(save_file=False)[0]
@@ -87,30 +84,46 @@ class TestVisualization:
         # Define what we expect the layers list to be.
         expected_layers_list = [
                                     [
-                                        {'id': 'Patient'},
-                                    ],
-                                    [
-                                        {'id': 'Biospecimen', 'parents': ['Patient']},
-                                        {'id': 'Imaging'},
-                                    ],
-                                    [
-                                        {'id': 'ScRNA-seqLevel1', 'parents': ['Biospecimen']},
-                                        {'id': 'BulkRNA-seqAssay', 'parents': ['Biospecimen']},
-                                        {'id': 'ImagingChannels', 'parents': ['Imaging', 'Biospecimen']},
-                                    ],
-                                    [
-                                        {'id': 'ScRNA-seqLevel2', 'parents': ['ScRNA-seqLevel1']},
-                                    ],
-                                    [
-                                        {'id': 'ScRNA-seqLevel3', 'parents': ['ScRNA-seqLevel2']},
+                                        {
+                                        "id": "Patient",
+                                        "parents": [],
+                                        "direct_children": [
+                                            "Biospecimen"
                                         ],
-                                    [
-                                        {'id': 'ScRNA-seqLevel4', 'parents': ['ScRNA-seqLevel3']},
+                                        "children": [
+                                            "Biospecimen",
+                                            "BulkRNA-seqAssay"
+                                        ]
+                                        }
                                     ],
+                                    [
+                                        {
+                                        "id": "Biospecimen",
+                                        "parents": [
+                                            "Patient"
+                                        ],
+                                        "direct_children": [
+                                            "BulkRNA-seqAssay"
+                                        ],
+                                        "children": [
+                                            "BulkRNA-seqAssay"
+                                        ]
+                                        }
+                                    ],
+                                    [
+                                        {
+                                        "id": "BulkRNA-seqAssay",
+                                        "parents": [
+                                            "Biospecimen"
+                                        ],
+                                        "direct_children": [],
+                                        "children": []
+                                        }
+                                    ]
                                 ]
 
         # Get actual layers.
         actual_layers_list = json.loads(layers_str)
-        
+
         # Check.
         assert actual_layers_list == expected_layers_list
