87e40a73367147134e016bc467b092cc234b8835
linglp <linglp@umich.edu>
2022-10-19 17:49:52 -0700

    delete old comment

--- run_api.py
+++ run_api.py
@@ -16,5 +16,3 @@ if __name__ == "__main__":
     app = create_app()
     CORS(app, resources={r"*": {"origins": "*"}})
     app.run(host=host, port=port, debug=True)
-    #CORS(app, resources={r"*": {"origins": "*"}})
-    #app.run(port=3001, debug=True)
