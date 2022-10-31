8a6172e138aefbb2c15776ad9c7fbe2c329b3d19
linglp <linglp@umich.edu>
2022-10-31 13:19:28 -0400

    lint override_version.py

--- override_version.py
+++ override_version.py
@@ -1,13 +1,13 @@
 import toml
-import os 
+import os
 
 data = toml.load("pyproject.toml")
-#get release version 
-RELEASE_VERSION = os.getenv('RELEASE_VERSION')
+# get release version
+RELEASE_VERSION = os.getenv("RELEASE_VERSION")
 # Modify field
-data['tool']['poetry']['version']=RELEASE_VERSION
-print('the version number of this release is: ', RELEASE_VERSION)
-#override and save changes
-f = open("pyproject.toml",'w')
+data["tool"]["poetry"]["version"] = RELEASE_VERSION
+print("the version number of this release is: ", RELEASE_VERSION)
+# override and save changes
+f = open("pyproject.toml", "w")
 toml.dump(data, f)
 f.close()
