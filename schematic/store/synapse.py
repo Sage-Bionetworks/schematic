28638ee8a19a3c8237911131305def2142ba9753
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2022-12-06 14:50:00 -0700

    TEMP add print statement for tests

--- schematic/store/synapse.py
+++ schematic/store/synapse.py
@@ -805,6 +805,7 @@ class SynapseStorage(BaseStorage):
                                     manipulation = 'replace')
 
 
+        print(manifest_table_id, manifest, table_manifest)
         return manifest_table_id, manifest, table_manifest
 
     def uplodad_manifest_file(self, manifest, metadataManifestPath, datasetId, restrict_manifest, component_name = ''):
