873bc908dda03dfe2e356e96c9f7c35e9e7b8549
Thomas Schaffter <thomas.schaffter@gmail.com>
2022-12-16 22:44:51 +0000

    Add function workspace-install

--- dev-env.sh
+++ dev-env.sh
@@ -1,11 +1,16 @@
 # This file to be sourced in the terminal for development.
 
+function workspace-install {
+  poetry shell
+  poetry install
+}
+
 function workspace-welcome {
   echo "Welcome to the Schematic repository! 👋"
 }
 
 function workspace-initialize-env {
-  workspace-welcome
+  ws-welcome
 
   export PATH="/home/vscode/.local/bin:$PATH"
 }
