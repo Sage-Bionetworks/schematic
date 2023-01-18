9f689053494aa7399a7384defb18a792b6ce8928
Mialy DeFelice <mialy.defelice@sagebase.org>
2023-01-18 10:10:25 -0800

    WIP: Add skeleton code for data_model_jsonld refactor work

--- schematic/schemas/data_model_jsonld.py
+++ schematic/schemas/data_model_jsonld.py
@@ -1,25 +1,43 @@
-class DataModelJsonLD():
-	def __init__():
+class DataModelJsonLD(object):
+	'''
+	Interface to JSONLD_object
+	'''
+
+	def __init__(data_model_graph):
 
 
 	def generate_data_model_jsonld(self):
+		'''
+		Will call JSONLD_object class to create properties and classes in the process.
+		'''
+		pass
 
 class JSONLD_object():
 	'''
 	Decorator class design
+	Base decorator class.
 	'''
-	def __init__():
+	_template: template = None
+
+	def __init__(self, to_template) -> None:
 		self.to_template()
 
-	def to_template(self):
+	def _create_template(self):
 		'''
 		Returns jsonld_class_template or jsonld_property_template
 		'''
-		return
+		return self._template
 
-class JSONLD_property():
-	def __init__():
-		self.to_template()
+	@property
+	def to_template(self):
+		return self._template.to_template()
+
+class JSONLD_property(JSONLD_object):
+	'''
+	Property Decorator
+	'''
+	def to_template(self)
+		return JSONLD_property(self._template.to_template())
 
 	def explore_property():
 		return
@@ -27,9 +45,12 @@ class JSONLD_property():
 	def edit_property():
 		return
 
-class JSONLD_class():
-	def __init__():
-		self.to_template()
+class JSONLD_class(JSONLD_object):
+	'''
+	Class Decorator
+	'''
+	def to_template(self)
+		return JSONLD_class(self._template.to_template())
 
 	def explore_class():
 		return
