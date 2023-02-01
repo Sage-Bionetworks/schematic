bdca7af5311b6f3958483a5a9e59ec3ecf194713
andrewelamb <andrewelamb@gmail.com>
2023-02-01 09:59:50 -0800

    add date to dict

--- schematic/models/GE_Helpers.py
+++ schematic/models/GE_Helpers.py
@@ -127,6 +127,7 @@ class GreatExpectationsHelpers(object):
             "float": "expect_column_values_to_be_in_type_list",
             "str": "expect_column_values_to_be_of_type",
             "num": "expect_column_values_to_be_in_type_list",
+            "date": "expect_column_values_to_be_of_type",
             "recommended": "expect_column_values_to_not_match_regex_list",
             "protectAges": "expect_column_values_to_be_between",
             "unique": "expect_column_values_to_be_unique",
