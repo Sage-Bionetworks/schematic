ae8eef2fd833faf3ec3a76e88306e43ff26bf2b3
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2023-03-15 11:39:28 -0700

    revert error enforcement commit

--- tests/test_api.py
+++ tests/test_api.py
@@ -701,19 +701,19 @@ class TestValidationBenchmark():
         # URL of validtion endpoint
         endpoint_url = 'http://localhost:3001/v1/model/validate'
 
+        if MockComponent_attribute == 'Check Ages':
+            test_invalid_manifest.loc[0,MockComponent_attribute]  = '6550'
+        elif MockComponent_attribute == 'Check Date':
+            test_invalid_manifest.loc[0,MockComponent_attribute]   = 'October 21 2022'
+            test_invalid_manifest.loc[2,MockComponent_attribute]   = 'October 21 2022'
+        elif MockComponent_attribute == 'Check Unique':
+            test_invalid_manifest.loc[0,MockComponent_attribute]   = 'str2'
+
+
         # Isolate single attribute of interest, keep `Component` column
         single_attribute_manfiest = test_invalid_manifest[['Component', MockComponent_attribute]]
 
-        # Enforce error rate of 33% in the manfiest before extension
-        if MockComponent_attribute == 'Check Ages':
-            single_attribute_manfiest.loc[0,MockComponent_attribute]  = '6550'
-        elif MockComponent_attribute == 'Check Date':
-            single_attribute_manfiest.loc[0,MockComponent_attribute]   = 'October 21 2022'
-            single_attribute_manfiest.loc[2,MockComponent_attribute]   = 'October 21 2022'
-        elif MockComponent_attribute == 'Check Unique':
-            single_attribute_manfiest.loc[0,MockComponent_attribute]   = 'str2'
-
-        # Extend to ~1000 rows in size for performance test
+        # Extend to ~1000 rows in size to for performance test
         multi_factor = ceil(target_rows/single_attribute_manfiest.shape[0])
         large_manfiest = pd.concat([single_attribute_manfiest]*multi_factor, ignore_index = True)
 
