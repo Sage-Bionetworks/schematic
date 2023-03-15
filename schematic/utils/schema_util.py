9433072437bf635685229df55682be569676f471
Mialy DeFelice <mialy.defelice@sagebase.org>
2023-03-15 15:23:59 -0700

    WIP: add name conversions to utils

--- schematic/utils/schema_util.py
+++ schematic/utils/schema_util.py
@@ -3,8 +3,36 @@ General methods.
 
 '''
 
-def get_property_label_from_display_name():
-	return
+def get_property_label_from_display_name(display_name, strict_camel_case = False):
+        """Convert a given display name string into a proper property label string"""
+        """
+        label = ''.join(x.capitalize() or ' ' for x in display_name.split(' '))
+        label = label[:1].lower() + label[1:] if label else ''
+        """
+        # This is the newer more strict method
+        if strict_camel_case:
+            display_name = display_name.strip().translate({ord(c): "_" for c in string.whitespace})
+            label = inflection.camelize(display_name, uppercase_first_letter=False)
 
-def get_class_label_from_display_name():
-	return
+        # This method remains for backwards compatibility
+        else:
+            display_name = display_name.translate({ord(c): None for c in string.whitespace})
+            label = inflection.camelize(display_name.strip(), uppercase_first_letter=False)
+
+        return label
+
+def get_class_label_from_display_name(display_name, strict_camel_case = False):
+    """Convert a given display name string into a proper class label string"""
+    """
+    label = ''.join(x.capitalize() or ' ' for x in display_name.split(' '))"""
+    # This is the newer more strict method
+    if strict_camel_case:
+        display_name = display_name.strip().translate({ord(c): "_" for c in string.whitespace})
+        label = inflection.camelize(display_name, uppercase_first_letter=True)
+
+    # This method remains for backwards compatibility
+    else:
+        display_name = display_name.translate({ord(c): None for c in string.whitespace})
+        label = inflection.camelize(display_name.strip(), uppercase_first_letter=True)
+
+    return label
