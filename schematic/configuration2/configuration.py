1e3762c8ecb81488b3998c86122cca07cd2e7e66
andrewelamb <andrewelamb@gmail.com>
2023-05-11 08:18:26 -0700

    temp commit

---
+++ schematic/configuration2/configuration.py
@@ -0,0 +1,201 @@
+"""Configuration singleton for the Schematic Package"""
+
+from typing import Optional
+import os
+import yaml
+from .dataclasses import (
+    SynapseConfig,
+    ManifestConfig,
+    ModelConfig,
+    GoogleSheetsConfig,
+)
+
+
+class Configuration:
+    """
+    This class is used as a singleton by the rest of the package.
+    It is instantiated only once at the bottom of this file, and that
+     instance is imported by other modules
+    """
+
+    def __init__(self) -> None:
+        self.config_path: Optional[str] = None
+        self._synapse_config = SynapseConfig()
+        self._manifest_config = ManifestConfig()
+        self._model_config = ModelConfig()
+        self._google_sheets_config = GoogleSheetsConfig()
+
+    def load_config(self, config_path: str) -> None:
+        """Loads a user created config file and overwrites any defaults  listed in the file
+
+        Args:
+            config_path (str): The path to the config file
+        """
+        config_path = os.path.expanduser(config_path)
+        config_path = os.path.abspath(config_path)
+        self.config_path = config_path
+        with open(config_path, "r", encoding="utf-8") as file:
+            data = yaml.safe_load(file)
+        self._synapse_config = SynapseConfig(
+            **data.get("asset_store", {}).get("synapse", {})
+        )
+        self._manifest_config = ManifestConfig(**data.get("manifest", {}))
+        self._model_config = ModelConfig(**data.get("model", {}))
+        self._google_sheets_config = GoogleSheetsConfig(**data.get("google_sheets", {}))
+
+    def _normalize_path(self, path: str) -> str:
+        """
+
+        Args:
+            path (str): The path to normalize
+
+        Returns:
+            str: The normalized path
+        """
+
+        if self.config_path:
+            # Retrieve parent directory of the config to decode relative paths
+            parent_dir = os.path.dirname(self.config_path)
+        else:
+            # assume the parent dir would be the current work dir
+            parent_dir = os.getcwd()
+
+        # Ensure absolute file paths
+        if not os.path.isabs(path):
+            path = os.path.join(parent_dir, path)
+        # And lastly, normalize file paths
+        return os.path.normpath(path)
+
+    @property
+    def synapse_configuration_path(self) -> str:
+        """
+        Returns:
+            str: The path to the synapse configuration file
+        """
+        return self._normalize_path(self._synapse_config.config_basename)
+
+    @property
+    def synapse_manifest_basename(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._synapse_config.manifest_basename
+
+    @property
+    def synapse_master_fileview_id(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._synapse_config.master_fileview_id
+
+    @synapse_master_fileview_id.setter
+    def synapse_master_fileview_id(self, synapse_id: str) -> None:
+        """Sets the synapse_master_fileview_id
+
+        Args:
+            synapse_id (str): The synapse id to set
+        """
+        self._synapse_config.master_fileview_id = synapse_id
+
+    @property
+    def synapse_manifest_folder(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._synapse_config.manifest_folder
+
+    @property
+    def manifest_title(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._manifest_config.title
+
+    @property
+    def manifest_data_type(self) -> list[str]:
+        """
+        Returns:
+            list[str]:
+        """
+        return self._manifest_config.data_type
+
+    @property
+    def model_location(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._model_config.location
+
+    @property
+    def model_file_type(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._model_config.file_type
+
+    @property
+    def service_account_credentials_synapse_id(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._google_sheets_config.service_acct_creds_synapse_id
+
+    @property
+    def service_account_credentials_path(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._normalize_path(
+            self._google_sheets_config.service_acct_creds_basename
+        )
+
+    @property
+    def google_sheets_master_template_id(self) -> str:
+        """
+        Returns:
+            str:
+        """
+        return self._google_sheets_config.master_template_id
+
+    @property
+    def google_sheets_strict_validation(self) -> bool:
+        """
+        Returns:
+            bool:
+        """
+        return self._google_sheets_config.strict_validation
+
+    @property
+    def google_required_background_color(self) -> dict[str, float]:
+        """
+        Returns:
+            dict[str, float]:
+        """
+        return {
+            "red": 0.9215,
+            "green": 0.9725,
+            "blue": 0.9803,
+        }
+
+    @property
+    def google_optional_background_color(self) -> dict[str, float]:
+        """
+        Returns:
+            dict[str, float]:
+        """
+        return {
+            "red": 1.0,
+            "green": 1.0,
+            "blue": 0.9019,
+        }
+
+# This instantiates the singleton for the rest of the package
+CONFIG = Configuration()
---
+++ schematic/configuration2/dataclasses.py
@@ -0,0 +1,165 @@
+"""Pydantic dataclasses"""
+
+import re
+from pydantic.dataclasses import dataclass
+from pydantic import validator
+
+
+@dataclass()
+class SynapseConfig:
+    """
+    config_basename: teh basename of the synapse config file
+    manifest_basename: the name of downloaded manifest files
+    master_fileview_id: Synapse id for the master file view
+    manifest_folder: name of the folder manifests will be saved to locally
+    """
+
+    config_basename: str = "config_basename"
+    manifest_basename: str = "synapse_storage_manifest"
+    master_fileview_id: str = "syn23643253"
+    manifest_folder: str = "manifests"
+
+    @validator("master_fileview_id")
+    @classmethod
+    def validate_synapse_id(cls, value: str) -> str:
+        """Check if string is a valid synapse id
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value isn't a valid Synapse id
+
+        Returns:
+            (str): The input value
+        """
+        if not re.search("^syn[0-9]+", value):
+            raise ValueError(f"{value} is not a valid Synapse id")
+        return value
+
+    @validator("config_basename", "manifest_basename", "manifest_folder")
+    @classmethod
+    def validate_string_is_not_empty(cls, value: str) -> str:
+        """Check if string  is not empty(has at least one char)
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value is zero characters long
+
+        Returns:
+            (str): The input value
+        """
+        if len(value) == 0:
+            raise ValueError(f"{value} is an empty string")
+        return value
+
+
+@dataclass()
+class ManifestConfig:
+    """
+    title:
+    data_type:
+    """
+
+    title: str = "example"
+    data_type: list[str] = ["Biospecimen", "Patient"]
+
+    @validator("title")
+    @classmethod
+    def validate_string_is_not_empty(cls, value: str) -> str:
+        """Check if string  is not empty(has at least one char)
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value is zero characters long
+
+        Returns:
+            (str): The input value
+        """
+        if len(value) == 0:
+            raise ValueError(f"{value} is an empty string")
+        return value
+
+
+@dataclass()
+class ModelConfig:
+    """
+    location: location of the schema jsonld, either a path, ro url
+    file_type: one of ["local"]
+    """
+
+    location: str = "tests/data/example.model.jsonld"
+    file_type: str = "local"
+
+    @validator("title", "file_type")
+    @classmethod
+    def validate_string_is_not_empty(cls, value: str) -> str:
+        """Check if string  is not empty(has at least one char)
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value is zero characters long
+
+        Returns:
+            (str): The input value
+        """
+        if len(value) == 0:
+            raise ValueError(f"{value} is an empty string")
+        return value
+
+
+@dataclass()
+class GoogleSheetsConfig:
+    """
+    master_template_id:
+    strict_validation:
+    service_acct_creds_synapse_id:
+    service_acct_creds_basename:
+    """
+
+    service_acct_creds_synapse_id: str = "syn25171627"
+    service_acct_creds_basename: str = "schematic_service_account_creds.json"
+    master_template_id: str = "1LYS5qE4nV9jzcYw5sXwCza25slDfRA1CIg3cs-hCdpU"
+    strict_validation: bool = True
+
+    @validator("master_template_id", "service_acct_creds_basename")
+    @classmethod
+    def validate_string_is_not_empty(cls, value: str) -> str:
+        """Check if string  is not empty(has at least one char)
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value is zero characters long
+
+        Returns:
+            (str): The input value
+        """
+        if len(value) == 0:
+            raise ValueError(f"{value} is an empty string")
+        return value
+
+    @validator("service_acct_creds_synapse_id")
+    @classmethod
+    def validate_synapse_id(cls, value: str) -> str:
+        """Check if string is a valid synapse id
+
+        Args:
+            value (str): A string
+
+        Raises:
+            ValueError: If the value isn't a valid Synapse id
+
+        Returns:
+            (str): The input value
+        """
+        if not re.search("^syn[0-9]+", value):
+            raise ValueError(f"{value} is not a valid Synapse id")
+        return value
