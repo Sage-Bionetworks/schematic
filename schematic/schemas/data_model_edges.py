2a5b0e28c117ee313bb8039834a2a43e7040cc0c
Mialy DeFelice <mialy.defelice@sagebase.org>
2023-06-28 11:23:09 -0700

    add weights to edges

--- schematic/schemas/data_model_edges.py
+++ schematic/schemas/data_model_edges.py
@@ -13,20 +13,30 @@ class DataModelEdges():
             # Get the relationships for the current attribure
             relationships = relationship['Relationships']
             # For each edge relationship
-            
             for key, csv_header in edge_relationships.items():
-                #if node == 'Patient' and attribute_display_name == 'HTAN Participant ID' and csv_header == 'Parent':
-                #    breakpoint()
                 # For a given relationship in the model
                 if csv_header in relationships.keys():
-                    # if the current node is part of that relationship and is not the current node
-                    if node in relationships[csv_header] and node != attribute_display_name:
-                        #print('Creating edge relationship \"' + csv_header +'\" with node ' + node + ' and attribute ' + attribute_display_name)
-                        # Connect node to attribute as an edge.
-                        G.add_edge(all_node_dict[node]['label'], all_node_dict[attribute_display_name]['label'], key=key)
+                    # If the current node is part of that relationship and is not the current node
+                    # Connect node to attribute as an edge.
+                    if node in relationships[csv_header] and node != attribute_display_name: 
+                        # Find position of node in the list, this is the weight
+                        # This will help us ensure things like valid values, or depends on are preserved in the proper order.
+                        
+                        # TODO: Move adding weights to its own helper.
+                        # TODO: create a new attribute in the rel dictionary looking for directionality. Save as out for domainIncludes, save as in for others.
+                        if key == 'domainIncludes':
+                            # Get weight from the order of the attributes.
+                            weight = list(data_model.keys()).index(attribute_display_name)
+                        elif type(relationships[csv_header]) == list:
+                            weight = relationships[csv_header].index(node)
+                        else:
+                            weight = 0
+                        # Here the first added node to the edge is the value that would be the valid value to the second node which is the attribute.
+                        G.add_edge(all_node_dict[node]['label'], all_node_dict[attribute_display_name]['label'], key=key, weight=weight)
                         # Add additional valid value edges
                         if key == 'rangeIncludes':
-                            G.add_edge(all_node_dict[attribute_display_name]['label'], all_node_dict[node]['label'],  key='subClassOf')
+                            # Add this relationships for classes.
+                            G.add_edge(all_node_dict[attribute_display_name]['label'], all_node_dict[node]['label'],  key='subClassOf', weight=weight)
 
         return G
 
