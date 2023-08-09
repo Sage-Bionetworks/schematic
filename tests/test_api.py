ce8c4469abc6854414c1d775968c2a56b2b1aa99
linglp <linglp@umich.edu>
2023-08-09 14:38:09 -0400

    edit test; also add test case to make sure that non-file base component manifest is correct

--- tests/test_api.py
+++ tests/test_api.py
@@ -524,12 +524,17 @@ class TestManifestOperation:
         # open the google sheet 
         google_sheet_df = pd.read_csv(response_google_sheet[0] + '/export?gid=0&format=csv')
         
-        # make sure that columns for annotations show up and entityId is at the end
-        assert google_sheet_df.columns.to_list() == ['Filename', 'Sample ID', 'File Format', 'Component', 'Genome Build', 'Genome FASTA', 'impact', 'Year of Birth', 'date', 'confidence', 'IsImportantBool', 'IsImportantText', 'author', 'eTag', 'entityId']
+        # make sure that columns used in annotations get added
+        # and also make sure that entityId column appears in the end
+        expected_list = ['Filename', 'Sample ID', 'File Format', 'Component', 'Genome Build', 'Genome FASTA', 'impact', 'Year of Birth', 'date', 'confidence', 'IsImportantBool', 'IsImportantText', 'author', 'eTag', 'entityId']
+        assert google_sheet_df.columns.to_list()[-1] == "entityId"
+        assert sorted(google_sheet_df.columns.to_list()) == sorted(expected_list)
 
-        # make sure Filename and entityId get filled with correct value
+        # make sure Filename, entityId, and component get filled with correct value
         assert google_sheet_df["Filename"].to_list() == ["TestDataset-Annotations-v3/Sample_A.txt", "TestDataset-Annotations-v3/Sample_B.txt", "TestDataset-Annotations-v3/Sample_C.txt"]
         assert google_sheet_df["entityId"].to_list() == ["syn25614636", "syn25614637", "syn25614638"]
+        assert google_sheet_df["Component"].to_list() == ["BulkRNA-seqAssay", "BulkRNA-seqAssay", "BulkRNA-seqAssay"]
+
 
     # test case: generate a manifest with annotations when use_annotations is set to False for a file-based component
     # the dataset folder does not contain an existing manifest 
@@ -553,11 +558,33 @@ class TestManifestOperation:
         # make sure that Filename and entityId gets filled 
         assert google_sheet_df["Filename"].to_list() == ["Sample_A.txt", "Sample_B.txt", "Sample_C.txt"]
         assert google_sheet_df["entityId"].to_list() == ["syn25614636", "syn25614637", "syn25614638"]
+        assert google_sheet_df["Component"].to_list() == ["BulkRNA-seqAssay", "BulkRNA-seqAssay", "BulkRNA-seqAssay"]
 
         # make sure that no annotations (no extra column) get added
-        assert google_sheet_df.columns == ['Filename', 'Sample ID', 'File Format', 'Component', 'Genome Build', 'Genome FASTA', 'entityId']
+        assert sorted(google_sheet_df.columns) == sorted(['Filename', 'Sample ID', 'File Format', 'Component', 'Genome Build', 'Genome FASTA', 'entityId'])
 
+    # test case: generate a manifest with annotations when use_annotations is set to True for a component that is not file-based
+    # the dataset folder does not contain an existing manifest 
+    def test_generate_manifest_not_file_based_with_annotations(self, client, data_model_jsonld):
+        params = {
+            "schema_url": data_model_jsonld,
+            "data_type": "BulkRNA-seqAssay",
+            "dataset_id": "syn25614635",
+            "asset_view": "syn51707141",
+            "output_format": "google_sheet", 
+            "use_annotations": False        
+        }
+        response = client.get('http://localhost:3001/v1/manifest/generate', query_string=params)
+        assert response.status_code == 200
 
+        response_google_sheet = json.loads(response.data)
+
+        # open the google sheet 
+        google_sheet_df = pd.read_csv(response_google_sheet[0] + '/export?gid=0&format=csv')
+
+        # make sure that the result is basically the same as generating a new manifest
+        assert google_sheet_df.columns == ['Patient ID', 'Sex', 'Year of Birth', 'Diagnosis', 'Component', 'Cancer Type', 'Family History']
+        
     def test_populate_manifest(self, client, data_model_jsonld, test_manifest_csv):
         # test manifest
         test_manifest_data = open(test_manifest_csv, "rb")
