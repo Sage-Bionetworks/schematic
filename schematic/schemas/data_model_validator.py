45eff995a70f0f74c0498f5d481b5b0bb6799226
Mialy DeFelice <mialy.defelice@sagebase.org>
2023-09-11 14:20:02 -0700

    clean/document schematic/schemas/data_model_validator.py

--- schematic/schemas/data_model_validator.py
+++ schematic/schemas/data_model_validator.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from typing import Any, Dict, Optional, Text, List,Tuple
 
 from schematic.schemas.data_model_relationships import (
 	DataModelRelationships
@@ -10,18 +11,25 @@ class DataModelValidator():
 	'''
 	def __init__(
 		self,
-		graph,
+		graph: nx.MultiDiGraph,
 		):
 		'''
+		Args:
+			graph, nx.MultiDiGraph: Graph representation of the data model.
 		TODO: put blacklisted chars and reserved_names in some global space where they can be accessed centrally
 		'''
 		self.graph = graph
 		self.DMR = DataModelRelationships()
-		# Removed check for spaces in display name since we get rid of those.
+		# Define blacklisted characters, taken from store.synapse
 		self.blacklisted_chars = ['(', ')', '.', '-']
+		# Define reserved_names, taken from Documentation
 		self.reserved_names = {'entityId'}
 
-	def run_checks(self):
+	def run_checks(self)->Tuple[list,list]:
+		""" Run all validation checks on the data model graph.
+		Returns, tuple(list, list): Returns a tuple of errors and warnings generated.
+		TODO: In future could design a way for groups to customize tests run for their groups, run additional tests, or move some to issuing only warnings, vice versa.
+		"""
 		error_checks = [
 					self.check_graph_has_required_node_fields(),
 					self.check_is_dag(),
@@ -32,11 +40,12 @@ class DataModelValidator():
 					]
 		errors = [error for error in error_checks if error]
 		warnings = [warning for warning in warning_checks if warning]
-		return errors
+		return errors, warnings
 
-	def check_graph_has_required_node_fields(self):
-		'''
-		Checks that each node is assigned a label.
+	def check_graph_has_required_node_fields(self)-> List[str]:
+		'''Checks that the graph has the required node fields for all nodes.
+		Returns:
+			error, list: List of error messages for each missing field.
 		'''
 		# Get all the fields that should be recorded per node
 		rel_dict = self.DMR.relationships_dictionary
@@ -47,27 +56,19 @@ class DataModelValidator():
 
 		error = []
 		missing_fields = []
-		# Check that nodes have labels
+		# Check that required fields are present for each node.
 		for node, node_dict in self.graph.nodes(data=True):
 			missing_fields.extend([(node, f) for f in node_fields if f not in node_dict.keys()])
 
 		if missing_fields:
-			for nf in missing_fields:
-				error.append(f'For entry: {nf[0]}, the required field {nf[1]} is missing in the data model graph, please double check your model and generate the graph again.')
+			for mf in missing_fields:
+				error.append(f'For entry: {mf[0]}, the required field {mf[1]} is missing in the data model graph, please double check your model and generate the graph again.')
 		return error
 
-	def check_is_dag(self):
-		'''
-		TODO:
-			- Check with Milen. This might be too simple of a check.
-			- Try wit topological sort as well. Benchmark against current approach.
-			- Add unit test to verify this works properly.
-
-		
-		if nx.number_of_selfloops(self.graph)!=0 and nx.is_directed(self.graph) == False:
-			error = f'Schematic requires that data models are Directed Acyclic Graphs (DAGs). ' \
-					f'Model supplied is not a DAG, please check your model.'
-			return error
+	def check_is_dag(self) -> List[str]:
+		'''Check that generated graph is a directed acyclic graph
+		Returns:
+			error, list: List of error messages if graph is not a DAG. List will include a message for each cycle found, if not there is a more generic message for the graph as a whole.
 		'''
 		error = []
 		if not nx.is_directed_acyclic_graph(self.graph):
@@ -80,8 +81,10 @@ class DataModelValidator():
 				error.append(f'Schematic requires models be a directed acyclic graph (DAG). Your graph is not a DAG, we could not locate the sorce of the error, please inspect your model.')
 		return error
 
-	def check_blacklisted_characters(self):
+	def check_blacklisted_characters(self) -> List[str]:
 		""" We strip these characters in store, so not sure if it matter if we have them now, maybe add warning 
+		Returns:
+			warning, list: list of warnings for each node in the graph, that has a Display name that contains blacklisted characters.
 		"""
 		warning = []
 		for node, node_dict in self.graph.nodes(data=True):
@@ -92,9 +95,10 @@ class DataModelValidator():
 				warning.append(f'Node: {node_display_name} contains a blacklisted character(s): {blacklisted_characters_str}, they will be striped if used in Synapse annotations.')
 		return warning
 
-	def check_reserved_names(self):
-		'''
-		# TODO: the error message is odd, what are the actual names that should be used? Not attribute or componenet...
+	def check_reserved_names(self) -> List[str]:
+		'''Identify if any names nodes in the data model graph are the same as reserved name.
+		Returns:
+			error, list: List of erros for every node in the graph whose name overlaps with the reserved names.
 		'''
 		error = []
 		reserved_names_found = [(name, node) for node in self.graph.nodes
