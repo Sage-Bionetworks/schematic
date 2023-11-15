d19bf0390f7fc8288e08e05c4878b89bede7adf5
linglp <linglp@umich.edu>
2023-11-15 14:13:45 -0500

    release version

--- override_version.py
+++ override_version.py
@@ -5,7 +5,7 @@ data = toml.load("pyproject.toml")
 #get release version 
 # RELEASE_VERSION = os.getenv('RELEASE_VERSION')
 # temporarily hard coded release version 
-RELEASE_VERSION = 'v23.11.2bN'
+RELEASE_VERSION = 'v23.11.dev2'
 # Modify field
 data['tool']['poetry']['version']=RELEASE_VERSION
 print('the version number of this release is: ', RELEASE_VERSION)
