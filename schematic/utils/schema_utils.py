5673776702fc585f6f94b307dd6c2456bd6a0a9b
andrewelamb <andrewelamb@gmail.com>
2024-02-14 08:41:22 -0800

    cleaned up pylint issues

--- schematic/utils/schema_utils.py
+++ schematic/utils/schema_utils.py
@@ -1,9 +1,13 @@
-import inflection
+"""Schema utils"""
+
+# pylint: disable=logging-fstring-interpolation
+
 import json
 import logging
-import networkx as nx
 import string
-from typing import Dict, List, Literal
+from typing import Literal, Optional
+
+import inflection
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +15,15 @@ DisplayLabelType = Literal["class_label", "display_label"]
 BLACKLISTED_CHARS = ["(", ")", ".", " ", "-"]
 
 
-def attr_dict_template(key_name: str) -> Dict[str, dict[str, dict]]:
+def attr_dict_template(key_name: str) -> dict[str, dict[str, dict]]:
+    """Returns a dict with a nested empty dict
+
+    Args:
+        key_name (str): The name of the key in the outer dict
+
+    Returns:
+        Dict[str, dict[str, dict]]: The dict
+    """
     return {key_name: {"Relationships": {}}}
 
 
@@ -21,7 +33,8 @@ def get_property_label_from_display_name(
     """Convert a given display name string into a proper property label string
     Args:
         display_name, str: node display name
-        strict_camel_case, bool: Default, False; defines whether or not to use strict camel case or not for conversion.
+        strict_camel_case, bool: Default, False; defines whether or not to use
+          strict camel case or not for conversion.
     Returns:
         label, str: property label of display name
     """
@@ -46,7 +59,8 @@ def get_class_label_from_display_name(
     """Convert a given display name string into a proper class label string
     Args:
         display_name, str: node display name
-        strict_camel_case, bool: Default, False; defines whether or not to use strict camel case or not for conversion.
+        strict_camel_case, bool: Default, False; defines whether or not to use
+          strict camel case or not for conversion.
     Returns:
         label, str: class label of display name
     """
@@ -66,12 +80,15 @@ def get_class_label_from_display_name(
 
 
 def get_attribute_display_name_from_label(
-    node_name: str, attr_relationships: dict
+    node_name: str, attr_relationships: dict[str, str]
 ) -> str:
-    """Get attribute display name for a node, using the node label, requires the attr_relationships dicitonary from the data model parser
+    """
+    Get attribute display name for a node, using the node label, requires the attr_relationships
+      dictionary from the data model parser
     Args:
         node_name, str: node label
-        attr_relationships, dict: dictionary defining attributes and relationships, generated in data model parser.
+        attr_relationships, dict: dictionary defining attributes and relationships,
+          generated in data model parser.
     Returns:
         display_name, str: node display name, recorded in attr_relationships.
     """
@@ -84,15 +101,18 @@ def get_attribute_display_name_from_label(
 
 def check_if_display_name_is_valid_label(
     display_name: str,
-    blacklisted_chars: list[str] = BLACKLISTED_CHARS,
+    blacklisted_chars: Optional[list[str]] = None,
 ) -> bool:
     """Check if the display name can be used as a display label
     Args:
         display_name, str: node display name
-        blacklisted_chars, list[str]: characters that are not permitted for synapse annotations uploads.
+        blacklisted_chars, Optional[list[str]]: characters that are not permitted
+          for synapse annotations uploads.
     Returns:
         valid_label, bool: True, if the display name can be used as a label, False, if it cannot.
     """
+    if blacklisted_chars is None:
+        blacklisted_chars = BLACKLISTED_CHARS
     valid_label = True
     if any(map(display_name.__contains__, blacklisted_chars)):
         valid_label = False
@@ -102,16 +122,20 @@ def check_if_display_name_is_valid_label(
 def get_stripped_label(
     display_name: str,
     entry_type: str,
-    blacklisted_chars: list[str] = BLACKLISTED_CHARS,
+    blacklisted_chars: Optional[list[str]] = None,
 ) -> str:
     """
     Args:
         display_name, str: node display name
         entry_type, str: 'class' or 'property', defines what type the entry is.
-        blacklisted_chars, list[str]: characters that are not permitted for synapse annotations uploads.
+        blacklisted_chars, list[str]: characters that are not permitted for synapse
+          annotations uploads.
     Returns:
-        stripped_label, str: class or property label that has been stripped of blacklisted characters.
+        stripped_label, str: class or property label that has been stripped of
+          blacklisted characters.
     """
+    if blacklisted_chars is None:
+        blacklisted_chars = BLACKLISTED_CHARS
     if entry_type.lower() == "class":
         stripped_label = [
             get_class_label_from_display_name(str(display_name)).translate(
@@ -127,7 +151,12 @@ def get_stripped_label(
         ][0]
 
     logger.warning(
-        f"Cannot use display name {display_name} as the data model label, becaues it is not formatted properly. Please remove all spaces and blacklisted characters: {str(blacklisted_chars)}. The following label was assigned instead: {stripped_label}"
+        (
+            f"Cannot use display name {display_name} as the data model label, "
+            "because it is not formatted properly. "
+            f"Please remove all spaces and blacklisted characters: {str(blacklisted_chars)}. "
+            f"The following label was assigned instead: {stripped_label}"
+        )
     )
     return stripped_label
 
@@ -139,7 +168,8 @@ def get_schema_label(
     Args:
         display_name, str: node display name
         entry_type, str: 'class' or 'property', defines what type the entry is.
-        strict_camel_case, bool: Default, False; defines whether or not to use strict camel case or not for conversion.
+        strict_camel_case, bool: Default, False; defines whether or not to use
+          strict camel case or not for conversion.
     Returns:
         label, str: class label of display name
     Raises:
@@ -156,7 +186,10 @@ def get_schema_label(
         )
     else:
         raise ValueError(
-            f"The entry type submitted: {entry_type}, is not one of the permitted types: 'class' or 'property'"
+            (
+                f"The entry type submitted: {entry_type}, is not one of the permitted types: "
+                "'class' or 'property'"
+            )
         )
     return label
 
@@ -171,7 +204,8 @@ def get_label_from_display_name(
     Args:
         display_name, str: node display name
         entry_type, str: 'class' or 'property', defines what type the entry is.
-        strict_camel_case, bool: Default, False; defines whether or not to use strict camel case or not for conversion.
+        strict_camel_case, bool: Default, False; defines whether or not to use strict camel 
+          case or not for conversion.
     Returns:
         label, str: label to be used for the provided display name.
     """
@@ -207,7 +241,7 @@ def convert_bool_to_str(provided_bool: bool) -> str:
     return str(provided_bool)
 
 
-def parse_validation_rules(validation_rules: List[str]) -> List[str]:
+def parse_validation_rules(validation_rules: list[str]) -> list[str]:
     """Split multiple validation rules based on :: delimiter
     Args:
         validation_rules, list: list containing a string validation rule
@@ -225,20 +259,23 @@ def export_schema(schema: dict, file_path: str) -> None:
         schema, dict: JSONLD schema
         filepath, str: path to store the schema
     """
-    with open(file_path, "w") as f:
-        json.dump(schema, f, sort_keys=True, indent=4, ensure_ascii=False)
+    with open(file_path, "w", encoding="utf-8") as schema_file:
+        json.dump(schema, schema_file, sort_keys=True, indent=4, ensure_ascii=False)
 
 
-def strip_context(context_value: str) -> tuple[str]:
+def strip_context(context_value: str) -> tuple[str, str]:
     """Strip contexts from str entry.
+
     Args:
-        context_value, str: string from which to strip context from
+        context_value (str): string from which to strip context from
+
     Returns:
-        context, str: the original context
-        v, str: value separated from context
+        tuple[str, str]:
+          The first string is the the original context
+          The second string is the value separated from context
     """
     if ":" in context_value:
-        context, v = context_value.split(":")
+        context, value = context_value.split(":")
     elif "@" in context_value:
-        context, v = context_value.split("@")
-    return context, v
+        context, value = context_value.split("@")
+    return context, value
