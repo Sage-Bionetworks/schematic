4ea095a711e7efe4b16cfc6471ef1bb292559a6e
Gianna Jordan <61707471+GiaJordan@users.noreply.github.com>
2024-02-14 13:27:35 -0700

    Revert "fix jsonschema validation"
    This reverts commit 85c1443ed0bb311b1dc12fbf8516ec347cb43b9a.

--- schematic/models/validate_manifest.py
+++ schematic/models/validate_manifest.py
@@ -264,7 +264,7 @@ class ValidateManifest(object):
         warnings = []
         col_attr = {}  # save the mapping between column index and attribute name
 
-        # numerical and null values need to be type string for the jsonValidator
+        # numerical values need to be type string for the jsonValidator
         for col in manifest.select_dtypes(
             include=[int, np.int64, float, np.float64]
         ).columns:
@@ -272,7 +272,6 @@ class ValidateManifest(object):
         manifest = manifest.map(
             lambda x: str(x) if isinstance(x, Number) else x, na_action="ignore"
         )
-        manifest.fillna("", inplace=True)
 
         annotations = json.loads(manifest.to_json(orient="records"))
         for i, annotation in enumerate(annotations):
