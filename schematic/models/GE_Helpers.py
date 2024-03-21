4ff56a0a1f199bf3cd06bd66156a4ba87542c39e
Mialy DeFelice <85905780+mialy-defelice@users.noreply.github.com>
2024-03-20 20:56:11 -0700

    change name to matchNone

--- schematic/models/GE_Helpers.py
+++ schematic/models/GE_Helpers.py
@@ -151,7 +151,7 @@ class GreatExpectationsHelpers(object):
             # "url": "expect_column_values_to_be_valid_urls",
             # "matchAtLeastOne": "expect_foreign_keys_in_column_a_to_exist_in_column_b",
             # "matchExactlyOne": "expect_foreign_keys_in_column_a_to_exist_in_column_b",
-            # "matchUnique": "expect_compound_columns_to_be_unique",
+            # "matchNone": "expect_compound_columns_to_be_unique",
         }
 
         # create blank expectation suite
