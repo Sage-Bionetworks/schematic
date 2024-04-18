f04777fda811e41502027351d291ac19125a40b3
Mialy DeFelice <85905780+mialy-defelice@users.noreply.github.com>
2024-04-18 09:49:49 -0700

    provide the validation rules to get_component_node_required for a light speed up

--- schematic/schemas/data_model_json_schema.py
+++ schematic/schemas/data_model_json_schema.py
@@ -194,7 +194,9 @@ class DataModelJSONSchema:
 
                 # Get if the node is required for the given component
                 node_required = self.dmge.get_component_node_required(
-                    manifest_component=source_node, node_display_name=node_display_name
+                    manifest_component=source_node,
+                    node_validation_rules=node_validation_rules,
+                    node_display_name=node_display_name
                 )
 
                 if node_display_name in reverse_dependencies:
