684671ca8749c75383ee8cbeaa974a736b8e75c9
Andrew Lamb <andrewelamb@gmail.com>
2024-04-18 08:07:26 -0700

    temp commit

--- set_release_version.py
+++ set_release_version.py
@@ -6,8 +6,10 @@ Changes pyproject.toml file to have thew release date
 import os
 from datetime import date
 import json
-import toml
 import re
+
+import toml
+
 
 TODAY = date.today()
 TODAYS_YEAR = str(TODAY.year)[-2:]
@@ -18,21 +20,11 @@ assert isinstance(tags, str)
 tag_list = json.loads(tags)
 assert isinstance(tag_list, list)
 
+NUM_VERSIONS = 0
 ref_list: list[str] = [tag_dict["ref"] for tag_dict in tag_list]
 for ref in ref_list:
     version = os.path.basename(ref)
-    print(version)
-    print(re.match("^v[0-9]+\.[0-9]+\.[0-9]+$", version))
-
-
-
-
-'''
-version_strings = [os.path.basename(tag_dict["ref"]) for tag_dict in tag_list]
-NUM_VERSIONS = 0
-for string in version_strings:
-    year, month, number = string.split(".")
-    if year == TODAYS_YEAR and month == TODAYS_MONTH:
+    if re.match("^v[0-9]+\.[0-9]+\.[0-9]+$", version):
         NUM_VERSIONS += 1
 
 RELEASE_VERSION = f"{TODAYS_YEAR}.{TODAYS_MONTH}.{NUM_VERSIONS + 1}"
@@ -44,4 +36,3 @@ print('the version number of this release is: ', RELEASE_VERSION)
 f = open("pyproject.toml",'w')
 toml.dump(data, f)
 f.close()
-'''
