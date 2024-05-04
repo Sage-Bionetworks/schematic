5d056bf9593ed1e30a5077661a3a2c410784a3f7
Thomas Yu <thomas.yu@sagebase.org>
2024-05-04 00:59:27 -0700

    Add schematic docker images

---
+++ local_deployment/schematic/run_app.py
@@ -0,0 +1,13 @@
+from schematic_api.api import create_app
+import os
+
+if __name__ == "__main__":
+    # Get app configuration
+    host = os.environ.get("APP_HOST", "0.0.0.0")
+    port = os.environ.get("APP_PORT", "3001")
+    port = int(port)
+
+    # Launch app
+    app = create_app()
+    #TO DO: add a flag --debug to control debug parameter
+    app.run(host=host, port=port, debug=False)
