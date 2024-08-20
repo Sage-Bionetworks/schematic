3d5941c27f1a07112c2d762b308bf9e18f3a7c33
GiaJordan <gianna.jordan@sagebase.org>
2024-08-20 11:49:16 -0700

    add test for submission

--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -2,8 +2,9 @@
 
 import logging
 import os
-from typing import Optional, Generator
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Generator, Optional
 from unittest.mock import patch
 
 import pytest
@@ -13,6 +14,11 @@ from tests.conftest import Helpers
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
+
+
+@contextmanager
+def does_not_raise():
+    yield
 
 
 def metadata_model(helpers, data_model_labels):
@@ -143,3 +149,20 @@ class TestMetadataModel:
                     hide_blanks=hide_blanks,
                 )
                 assert mock_manifest_id == "mock manifest id"
+
+    def test_submit_filebased_manifest(self, helpers):
+        meta_data_model = metadata_model(helpers, "class_label")
+
+        manifest_path = helpers.get_data_path(
+            "mock_manifests/filepath_submission_test_manifest.csv"
+        )
+
+        with does_not_raise():
+            manifest_id = meta_data_model.submit_metadata_manifest(
+                manifest_path=manifest_path,
+                dataset_id="syn62276880",
+                manifest_record_type="file_only",
+                restrict_rules=False,
+                file_annotations_upload=True,
+            )
+            assert manifest_id == "syn62280543"
