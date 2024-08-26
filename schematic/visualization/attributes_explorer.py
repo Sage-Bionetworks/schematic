35c13ddaac04350c0c14c5c8d6c456e97fe95593
afwillia <anthony.williams@sagebase.org>
2024-08-26 14:33:03 -0700

    use if ... is not None instead of if not ...

--- schematic/visualization/attributes_explorer.py
+++ schematic/visualization/attributes_explorer.py
@@ -33,24 +33,24 @@ class AttributesExplorer:
         self.path_to_jsonld = path_to_jsonld
 
         self.jsonld = load_json(self.path_to_jsonld)
-        if data_model_graph_pickle and not graph_data_model:
+        if data_model_graph_pickle and graph_data_model is not None:
             self.graph_data_model = read_pickle(data_model_graph_pickle)
 
         # Parse Model
-        if not parsed_data_model:
+        if parsed_data_model is not None:
             data_model_parser = DataModelParser(
                 path_to_data_model=self.path_to_jsonld,
             )
             parsed_data_model = data_model_parser.parse_model()
 
         # Instantiate DataModelGraph
-        if not data_model_grapher:
+        if data_model_grapher is not None:
             data_model_grapher = DataModelGraph(parsed_data_model, data_model_labels)
             # Generate graph
             self.graph_data_model = data_model_grapher.graph
 
         # Instantiate Data Model Graph Explorer
-        if not data_model_graph_explorer:
+        if data_model_graph_explorer is not None:
             self.dmge = DataModelGraphExplorer(self.graph_data_model)
         else:
             self.dmge = data_model_graph_explorer
