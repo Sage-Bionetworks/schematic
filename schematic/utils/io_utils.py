05b4fe081e1456c73ee2119a38ffaf9323519537
afwillia <anthony.williams@sagebase.org>
2024-08-26 14:05:39 -0700

    Add read_pickle function to read a binary pickle file

--- schematic/utils/io_utils.py
+++ schematic/utils/io_utils.py
@@ -4,6 +4,7 @@ from typing import Any
 import json
 import urllib.request
 from schematic import LOADER
+import pickle
 
 
 def load_json(file_path: str) -> Any:
@@ -40,3 +41,9 @@ def load_schemaorg() -> Any:
     data_path = "data_models/schema_org.model.jsonld"
     schema_org_path = LOADER.filename(data_path)
     return load_json(schema_org_path)
+
+def read_pickle():
+    """Read pickle file"""
+    with open(file_path, "rb") as fle:
+        data = pickle.load(fle)
+        return data
