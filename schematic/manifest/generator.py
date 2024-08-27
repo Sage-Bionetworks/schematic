23b7e1d6010c3958548edbca8f3eba62d8d2ffa8
afwillia <anthony.williams@sagebase.org>
2024-08-27 13:41:51 -0700

    update docstring and use if ... is not None instead of if not ...

--- schematic/manifest/generator.py
+++ schematic/manifest/generator.py
@@ -1650,7 +1650,7 @@ class ManifestGenerator(object):
 
         Args:
             path_to_data_model (str): str path to data model
-            data_model_graph_pickle (str): A data model graph as csv or pickle
+            data_model_graph_pickle (str): path to pickled networkx MultiDiGraph object
             graph_data_model (str): An networkx MultiDiGraph object
             data_types (list): a list of data types
             access_token (str, optional): synapse access token. Required when getting an existing manifest. Defaults to None.
@@ -1682,7 +1682,7 @@ class ManifestGenerator(object):
                     "Please check your submission and try again."
                 )
 
-        if not graph_data_model:
+        if graph_data_model is not None:
             if data_model_graph_pickle:
                 """What if pickle file does not fit in memory?"""
                 graph_data_model = read_pickle(data_model_graph_pickle)
