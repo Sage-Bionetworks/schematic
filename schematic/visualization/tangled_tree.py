795e2637b811785dc13a1b1fbd5b8d34334f6b19
afwillia <anthony.williams@sagebase.org>
2024-08-27 08:35:43 -0700

    fix pickle reading logic in tangled tree

--- schematic/visualization/tangled_tree.py
+++ schematic/visualization/tangled_tree.py
@@ -63,7 +63,7 @@ class TangledTree:  # pylint: disable=too-many-instance-attributes disable=too-m
         parsed_data_model = None
 
         # Instantiate Data Model Parser
-        if data_model_graph_pickle is not None:
+        if data_model_graph_pickle is None:
             data_model_parser = DataModelParser(
                 path_to_data_model=self.path_to_json_ld,
             )
