48f10c2a69bd9a159e10ed8ad342b780262eb69d
GiaJordan <gianna.jordan@sagebase.org>
2024-08-30 13:31:17 -0700

    update submission integration test

--- tests/integration/test_metadata_model.py
+++ tests/integration/test_metadata_model.py
@@ -11,7 +11,9 @@ logger = logging.getLogger(__name__)
 
 
 class TestMetadataModel:
-    def test_submit_filebased_manifest(self, helpers, mocker: MockerFixture):
+    def test_submit_filebased_manifest(
+        self, helpers, mocker: MockerFixture, synapse_store
+    ):
         # spys
         spy_upload_file_as_csv = mocker.spy(SynapseStorage, "upload_manifest_as_csv")
         spy_upload_file_as_table = mocker.spy(
@@ -25,16 +27,24 @@ class TestMetadataModel:
         # GIVEN a metadata model object using class labels
         meta_data_model = metadata_model(helpers, "class_label")
 
-        # AND a filebased test manifset
-        manifest_path = helpers.get_data_path(
-            "mock_manifests/filepath_submission_test_manifest.csv"
+        # AND a filebased test manifest
+        manifest_path = "mock_manifests/filepath_submission_test_manifest.csv"
+        load_args = {
+            "dtype": "string",
+        }
+        manifest = helpers.get_data_frame(
+            manifest_path, preserve_raw_input=False, **load_args
         )
+        manifest_full_path = helpers.get_data_path(manifest_path)
 
-        # WHEN the manifest it submitted
+        # AND datafiles on synapse that should be annotated
+        entityIds = manifest["entityId"].tolist()
+
+        # WHEN the manifest it submitted and files are annotated
         # THEN submission should complete without error
         with does_not_raise():
             manifest_id = meta_data_model.submit_metadata_manifest(
-                manifest_path=manifest_path,
+                manifest_path=manifest_full_path,
                 dataset_id="syn62276880",
                 manifest_record_type="file_and_entities",
                 restrict_rules=False,
@@ -42,14 +52,22 @@ class TestMetadataModel:
                 hide_blanks=False,
             )
 
-            # AND the manifest should be submitted to the correct place
-            assert manifest_id == "syn62280543"
+        # AND the files should be annotated
+        spy_add_annotations.assert_called_once()
+        # AND the annotations should have the correct metadata
+        for row in manifest.itertuples():
+            entityId = row.entityId
+            expected_sample_id = row._2
+            annos = synapse_store.syn.get_annotations(entityId)
+            sample_id = annos["SampleID"][0]
+            assert sample_id == expected_sample_id
 
-            # AND the manifest should be uploaded as a CSV
-            spy_upload_file_as_csv.assert_called_once()
-            # AND annotations should be added to the files
-            spy_add_annotations.assert_called_once()
+        # AND the manifest should be submitted to the correct place
+        assert manifest_id == "syn62280543"
 
-            # AND the manifest should not be uploaded as a table or combination of table, file, and entities
-            spy_upload_file_as_table.assert_not_called()
-            spy_upload_file_combo.assert_not_called()
+        # AND the manifest should be uploaded as a CSV
+        spy_upload_file_as_csv.assert_called_once()
+
+        # AND the manifest should not be uploaded as a table or combination of table, file, and entities
+        spy_upload_file_as_table.assert_not_called()
+        spy_upload_file_combo.assert_not_called()
