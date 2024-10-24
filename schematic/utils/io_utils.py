0bd946c3d4a1d953830a8cd1ee6c131c50dcd3ef
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-24 14:07:15 -0700

    [FDS-2386] Synapse entity tracking and code concurrency updates (#1505)
    * [FDS-2386] Synapse entity tracking and code concurrency updates

--- schematic/utils/io_utils.py
+++ schematic/utils/io_utils.py
@@ -1,9 +1,10 @@
 """io utils"""
 
-from typing import Any
 import json
+import os
+import time
 import urllib.request
-from schematic import LOADER
+from typing import Any
 
 
 def load_json(file_path: str) -> Any:
@@ -31,6 +32,9 @@ def export_json(json_doc: Any, file_path: str) -> None:
 def load_default() -> Any:
     """Load biolink vocabulary"""
     data_path = "data_models/biothings.model.jsonld"
+    # Lazy import to avoid circular imports
+    from schematic import LOADER  # pylint: disable=import-outside-toplevel
+
     biothings_path = LOADER.filename(data_path)
     return load_json(biothings_path)
 
@@ -38,5 +42,40 @@ def load_default() -> Any:
 def load_schemaorg() -> Any:
     """Load SchemaOrg vocabulary"""
     data_path = "data_models/schema_org.model.jsonld"
+    # Lazy import to avoid circular imports
+    from schematic import LOADER  # pylint: disable=import-outside-toplevel
+
     schema_org_path = LOADER.filename(data_path)
     return load_json(schema_org_path)
+
+
+def cleanup_temporary_storage(
+    temporary_storage_directory: str, time_delta_seconds: int
+) -> None:
+    """Handles cleanup of temporary storage directory. The usage of the
+    `time_delta_seconds` parameter is to prevent deleting files that are currently
+    being used by other requests. In production we will be deleting those files
+    which have not been modified for more than 1 hour.
+
+    Args:
+        temporary_storage_directory: Path to the temporary storage directory.
+        time_delta_seconds: The time delta in seconds used to determine which files
+            should be deleted.
+    """
+    if os.path.exists(temporary_storage_directory):
+        for root, all_dirs, files in os.walk(
+            temporary_storage_directory, topdown=False
+        ):
+            # Delete files older than the specified time delta
+            for file in files:
+                file_path = os.path.join(root, file)
+                if os.path.isfile(file_path) and os.path.getmtime(file_path) < (
+                    time.time() - time_delta_seconds
+                ):
+                    os.remove(file_path)
+
+            # Delete empty directories
+            for all_dir in all_dirs:
+                dir_path = os.path.join(root, all_dir)
+                if not os.listdir(dir_path):
+                    os.rmdir(dir_path)
