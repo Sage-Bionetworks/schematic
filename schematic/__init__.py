6a9fc2f26f2cbc8182b99ce29ee95c438004c1ca
BryanFauble <17128019+BryanFauble@users.noreply.github.com>
2024-10-29 12:33:30 -0700

    Add requests-oauth2client to supported oauth2 token exchange for authenticated export of telemetry data

--- schematic/__init__.py
+++ schematic/__init__.py
@@ -1,13 +1,18 @@
+import gzip
 import logging
 import os
 import time
+import zlib
+from io import BytesIO
 from typing import Dict, List
 
 import pkg_resources
+import requests
 from opentelemetry import trace
 from opentelemetry._logs import set_logger_provider
-from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.http import Compression
+from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
@@ -25,6 +30,7 @@ from opentelemetry.sdk.trace.export import (
     Span,
 )
 from opentelemetry.sdk.trace.sampling import ALWAYS_OFF
+from requests_oauth2client import OAuth2Client, OAuth2ClientCredentialsAuth
 from synapseclient import Synapse
 from werkzeug import Request
 
@@ -52,8 +58,48 @@ class FileSpanExporter(ConsoleSpanExporter):
                 f.write(span_json_one_line)
 
 
-def set_up_tracing() -> None:
-    """Set up tracing for the API."""
+def create_telemetry_session() -> requests.Session:
+    """
+    Create a requests session with authorization enabled if environment variables are set.
+    If no environment variables are set, the session will be created without authorization.
+    """
+    session = requests.Session()
+    client_id = os.environ.get("TELEMETRY_EXPORTER_CLIENT_ID", None)
+    client_secret = os.environ.get("TELEMETRY_EXPORTER_CLIENT_SECRET", None)
+    client_token_endpoint = os.environ.get(
+        "TELEMETRY_EXPORTER_CLIENT_TOKEN_ENDPOINT", None
+    )
+    client_audience = os.environ.get("TELEMETRY_EXPORTER_CLIENT_AUDIENCE", None)
+    if (
+        client_id is None
+        or client_secret is None
+        or client_token_endpoint is None
+        or client_audience is None
+    ):
+        logger.warning(
+            "No client_id, client_secret, client_audience, or token_endpoint provided for telemetry exporter. Telemetry data will be sent without authorization."
+        )
+        return session
+
+    oauth2client = OAuth2Client(
+        token_endpoint=client_token_endpoint,
+        client_id=client_id,
+        client_secret=client_secret,
+    )
+
+    auth = OAuth2ClientCredentialsAuth(oauth2client, audience=client_audience)
+    session.auth = auth
+
+    return session
+
+
+def set_up_tracing(session: requests.Session) -> None:
+    """Set up tracing for the API.
+    Args:
+        session: requests.Session object to use for exporting telemetry data. If
+            the exporter is set to OTLP, this session will be used to send the data.
+            If the exporter is set to file, this session will be ignored.
+    """
     tracing_export = os.environ.get("TRACING_EXPORT_FORMAT", None)
     if tracing_export is not None and tracing_export:
         Synapse.enable_open_telemetry(True)
@@ -76,9 +122,12 @@ def set_up_tracing() -> None:
         )
 
     if tracing_export == "otlp":
-        trace.get_tracer_provider().add_span_processor(
-            BatchSpanProcessor(OTLPSpanExporter())
-        )
+        import types
+
+        exporter = OTLPSpanExporter(session=session)
+        # This is a temporary hack
+        exporter._export = types.MethodType(trace_export_replacement, exporter)
+        trace.get_tracer_provider().add_span_processor(BatchSpanProcessor(exporter))
     elif tracing_export == "file":
         timestamp_millis = int(time.time() * 1000)
         file_name = f"otel_spans_integration_testing_{timestamp_millis}.ndjson"
@@ -89,7 +138,7 @@ def set_up_tracing() -> None:
         trace.set_tracer_provider(TracerProvider(sampler=ALWAYS_OFF))
 
 
-def set_up_logging() -> None:
+def set_up_logging(session: requests.Session) -> None:
     """Set up logging to export to OTLP."""
     logging_export = os.environ.get("LOGGING_EXPORT_FORMAT", None)
     logging_service_name = os.environ.get("LOGGING_SERVICE_NAME", "schematic-api")
@@ -105,7 +154,11 @@ def set_up_logging() -> None:
         logger_provider = LoggerProvider(resource=resource)
         set_logger_provider(logger_provider=logger_provider)
 
-        exporter = OTLPLogExporter()
+        exporter = OTLPLogExporter(session=session)
+        import types
+
+        # This is a temporary hack
+        exporter._export = types.MethodType(log_export_replacement, exporter)
         logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
         handler = LoggingHandler(level=logging.NOTSET, logger_provider=logger_provider)
         logging.getLogger().addHandler(handler)
@@ -150,5 +203,46 @@ def response_hook(span: Span, status: str, response_headers: List) -> None:
     pass
 
 
-set_up_tracing()
-set_up_logging()
+# This is a temporary hack to get around the fact that I am currently using self-signed SSL certs.
+# I have an IT ticket to get a domain to use for this purpose so I can request a proper SSL cert from LetsEncrypt.
+def trace_export_replacement(self, serialized_data: bytes):
+    data = serialized_data
+    if self._compression == Compression.Gzip:
+        gzip_data = BytesIO()
+        with gzip.GzipFile(fileobj=gzip_data, mode="w") as gzip_stream:
+            gzip_stream.write(serialized_data)
+        data = gzip_data.getvalue()
+    elif self._compression == Compression.Deflate:
+        data = zlib.compress(serialized_data)
+
+    return self._session.post(
+        url=self._endpoint,
+        data=data,
+        verify=False,
+        timeout=self._timeout,
+        cert=self._client_cert,
+    )
+
+
+def log_export_replacement(self, serialized_data: bytes):
+    data = serialized_data
+    if self._compression == Compression.Gzip:
+        gzip_data = BytesIO()
+        with gzip.GzipFile(fileobj=gzip_data, mode="w") as gzip_stream:
+            gzip_stream.write(serialized_data)
+        data = gzip_data.getvalue()
+    elif self._compression == Compression.Deflate:
+        data = zlib.compress(serialized_data)
+
+    return self._session.post(
+        url=self._endpoint,
+        data=data,
+        verify=False,
+        timeout=self._timeout,
+        cert=self._client_cert,
+    )
+
+
+request_session = create_telemetry_session()
+set_up_tracing(session=request_session)
+set_up_logging(session=request_session)
