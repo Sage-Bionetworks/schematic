514ecf85822c748047646aefd965e619a1bef59f
Lingling <55448354+linglp@users.noreply.github.com>
2025-02-24 15:10:27 -0500

    [SCHEMATIC-240] stripped google sheet information from open telemetry span status and message  (#1573)
    * catch google http error and sanitized error message
    
    * remove unncessary code
    
    * remove print
    
    * remove unused imports
    
    * add comment
    
    * add a custom log processor to remove sensitive information in the log
    
    * add span processor
    
    * use export
    
    * patch the _readable_span method in span class and remove processor
    
    * add test; move function to util
    
    * add module docstring and fix syntax
    
    * remove try except in code block
    
    * remove unnecessary import
    
    * added the space back
    
    * revert space changes
    
    * remove sensitive info in status description
    
    * modify attribute directly
    
    * remoove unused imports
    
    * strip sensitive info in log in a consistent way
    
    * remove unused import
    
    * update regex to accomodate different version of google sheet
    
    * update comments

---
+++ tests/unit/test_filter_sensitive_data.py
@@ -0,0 +1,41 @@
+import pytest
+from schematic.utils.remove_sensitive_data_utils import (
+    redact_string,
+    redacted_sensitive_data_in_exception,
+)
+
+
+class TestFilterSensitiveData:
+    @pytest.mark.parametrize(
+        "test_google_sheet",
+        [
+            "googleapiclient.errors.HttpError: <HttpError 400 when requesting https://sheets.googleapis.com/v4/spreadsheets/11234budyhf:batchUpdate?fields=%2A&alt=json returned abc>",
+            "googleapiclient.errors.HttpError: <HttpError 400 when requesting https://sheets.googleapis.com/v3/spreadsheets/11234budyhf:batchUpdate?fields=%2A&alt=json returned abc>",
+            "googleapiclient.errors.HttpError: <HttpError 400 when requesting https://sheets.googleapis.com/v10/spreadsheets/11234budyhf:batchUpdate?fields=%2A&alt=json returned abc>",
+        ],
+    )
+    def test_redact_string(self, test_google_sheet) -> None:
+        # given a string with sensitive data, make sure that they are redacted
+        redacted_data = redact_string(test_google_sheet)
+        assert (
+            redacted_data
+            == "googleapiclient.errors.HttpError: <HttpError 400 when requesting [REDACTED_GOOGLE_SHEETS]:batchUpdate?fields=%2A&alt=json returned abc>"
+        )
+
+    def test_redacted_sensitive_data_in_exception(self) -> None:
+        # given a dictionary of exception attributes, make sure that sensitive data is redacted
+        exception_attributes = {
+            "exception.message": "googleapiclient.errors.HttpError: <HttpError 400 when requesting https://sheets.googleapis.com/v4/spreadsheets/11234budyhf:batchUpdate?fields=%2A&alt=json returned>",
+            "exception.stacktrace": 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\n  File "<string>", line 1, in <module>\n  File "/usr/local/lib/python3.7/dist-packages/googleapiclient/_helpers.py", line 134, in positional_wrapper\n    return wrapped(*args, **kwargs)\n  File "/usr/local/lib/python3.7/dist-packages/googleapiclient/http.py", line 905, in execute\n    raise HttpError(resp, content, uri=self.uri)\ngoogleapiclient.errors.HttpError: <HttpError 400 when requesting https://sheets.googleapis.com/v4/spreadsheets/11234budyhf:batchUpdate?fields=%2A&alt=json returned>',
+        }
+        redacted_exception_attributes = redacted_sensitive_data_in_exception(
+            exception_attributes
+        )
+        assert (
+            redacted_exception_attributes["exception.message"]
+            == "googleapiclient.errors.HttpError: <HttpError 400 when requesting [REDACTED_GOOGLE_SHEETS]:batchUpdate?fields=%2A&alt=json returned>"
+        )
+        assert (
+            redacted_exception_attributes["exception.stacktrace"]
+            == 'Traceback (most recent call last):\n  File "<stdin>", line 1, in <module>\n  File "<string>", line 1, in <module>\n  File "/usr/local/lib/python3.7/dist-packages/googleapiclient/_helpers.py", line 134, in positional_wrapper\n    return wrapped(*args, **kwargs)\n  File "/usr/local/lib/python3.7/dist-packages/googleapiclient/http.py", line 905, in execute\n    raise HttpError(resp, content, uri=self.uri)\ngoogleapiclient.errors.HttpError: <HttpError 400 when requesting [REDACTED_GOOGLE_SHEETS]:batchUpdate?fields=%2A&alt=json returned>'
+        )
