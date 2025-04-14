e0551a17afd6b44b8ecca11b6f08faf5dc7c658c
GiaJordan <gianna.jordan@sagebase.org>
2025-04-14 11:53:37 -0700

    wip add bones of test for new command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -186,3 +186,50 @@ class TestSchemaCli:
             )
 
             assert result.exit_code == 0
+
+    # TODO: change url target to develop branch
+    @ pytest.mark.parametrize(
+        "data_model",
+        [
+            "example.model.jsonld",
+            "https://raw.githubusercontent.com/Sage-Bionetworks/schematic/refs/heads/schematic-276-jsonschema-cli/tests/data/example.model.jsonld",
+        ],
+        ids=["local", "remote"],
+    ) @ pytest.mark.parametrize(
+        "component",
+        ["MockComponent", None],
+        ids=["component specified", "all components"],
+    )
+    @pytest.mark.parametrize(
+        "output_path",
+        ["test/jsonschema_output/", None],
+        ids=["output directory specified", "no output directory specified"],
+    )
+    def test_json_schema_generation(
+        self,
+        runner: CliRunner,
+        helpers: Helpers,
+        config: Configuration,
+        data_model: str,
+        output_path: str,
+        component: str,
+    ):
+        if data_model.startswith("example"):
+            data_model = helpers.get_data_path(data_model)
+
+        config.load_config("config_example.yml")
+
+        result = runner.invoke(
+            schema,
+            [
+                "--config",
+                config.config_path,
+                "generate-jsonschema",
+                "--data_model",
+                data_model,
+                "--output_path",
+                output_path,
+                "--component",
+                component,
+            ],
+        )
