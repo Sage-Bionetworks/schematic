a2fed0920ef10ff698fcb4befdded5a72bfc74e4
GiaJordan <gianna.jordan@sagebase.org>
2025-04-16 10:30:40 -0700

    add tests for director

--- tests/unit/test_json_schema_component_generator.py
+++ tests/unit/test_json_schema_component_generator.py
@@ -1,7 +1,48 @@
 import pytest
 
+from pathlib import Path
+import os
+import filecmp
+
 from schematic.schemas.json_schema_component_generator import GeneratorDirector
 from tests.conftest import Helpers
+
+
+class TestGeneratorDirector:
+    @pytest.mark.parametrize("component", ["MockComponent"])
+    def test_generate_jsonschema(
+        self,
+        helpers,
+        component,
+    ):
+        output_directory = helpers.get_data_path("test_jsonschemas")
+        expected_jsonschema = helpers.get_data_path(
+            f"expected_jsonschemas/expected.{component}_validation_schema.json"
+        )
+
+        data_model = helpers.get_data_path("example.model.jsonld")
+        generator = GeneratorDirector(
+            data_model=data_model,
+            components=[component],
+            output_directory=output_directory,
+        )
+        json_schema = generator.generate_jsonschema()
+
+        generated_jsonschema = helpers.get_data_path(
+            f"{output_directory}/{component}_validation_schema.json"
+        )
+
+        assert json_schema is not None
+        assert isinstance(json_schema, list)
+
+        assert os.path.isfile(generated_jsonschema)
+        assert filecmp.cmp(
+            expected_jsonschema,
+            generated_jsonschema,
+            shallow=False,
+        )
+
+        return
 
 
 class TestJsonSchemaComponentGenerator:
