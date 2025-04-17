4e139c0be10d818fec6f0f4dcf78720ce16a7202
GiaJordan <gianna.jordan@sagebase.org>
2025-04-17 11:39:33 -0700

    add comments and test case

--- tests/unit/test_json_schema_component_generator.py
+++ tests/unit/test_json_schema_component_generator.py
@@ -52,12 +52,15 @@ def output_directory(helpers):
 class TestGeneratorDirector:
     @pytest.mark.parametrize("component", [(["MockComponent"], None)])
     def test_init(self, helpers, component, output_directory, data_model):
+        # GIVEN the GeneratorDirector class and certain parameters
+        # WHEN the class is initialized
         generator = GeneratorDirector(
             data_model=data_model,
             components=["MockComponent"],
             output_directory=output_directory,
         )
 
+        # THEN the class should be initialized with the correct parameters
         assert generator.data_model == data_model
         assert generator.components == ["MockComponent"]
         assert generator.output_directory == output_directory
@@ -90,20 +93,27 @@ class TestGeneratorDirector:
     ):
         comopnent_gather_spy = mocker.spy(GeneratorDirector, "gather_components")
 
+        # GIVEN a GeneratorDirector instance, a data model, an output directory, and optionally a component
         generator = GeneratorDirector(
             data_model=data_model,
             components=specified_component,
             output_directory=output_directory,
         )
+
+        # WHEN a schema is generated
         json_schema = generator.generate_jsonschema()
 
+        # AND if no component is specified already
+        # THEN all components should be gathered from the data model
         if specified_component is None:
             comopnent_gather_spy.assert_called_once()
 
+        # AND a list json schema(s) should be returned with entries for each component
         assert json_schema is not None
         assert isinstance(json_schema, list)
         assert len(json_schema) == len(expected_components)
 
+        # AND for each component generated
         for expected_component in expected_components:
             expected_component = expected_component.replace(" ", "")
 
@@ -115,7 +125,9 @@ class TestGeneratorDirector:
                 f"{output_directory}/{expected_component}_validation_schema.json"
             )
 
+            # A file should be written to the correct path
             assert os.path.isfile(generated_jsonschema)
+            # AND the JSON schema should match the expected schema
             assert json_files_equal(expected_jsonschema, generated_jsonschema)
 
     @pytest.mark.parametrize(
@@ -132,20 +144,120 @@ class TestGeneratorDirector:
                     "MockFilename",
                 ],
             ),
+            (
+                "https://raw.githubusercontent.com/ncihtan/data-models/refs/heads/main/HTAN.model.jsonld",
+                [
+                    "10x Visium Spatial Transcriptomics - Auxiliary Files",
+                    "10x Visium Spatial Transcriptomics - RNA-seq Level 1",
+                    "10x Visium Spatial Transcriptomics - RNA-seq Level 2",
+                    "10x Visium Spatial Transcriptomics - RNA-seq Level 3",
+                    "10x Visium Spatial Transcriptomics - RNA-seq Level 4",
+                    "10X Genomics Xenium ISS Experiment",
+                    "Bulk Methylation-seq Level 1",
+                    "Bulk Methylation-seq Level 2",
+                    "Bulk Methylation-seq Level 3",
+                    "Bulk RNA-seq Level 1",
+                    "Bulk RNA-seq Level 2",
+                    "Bulk RNA-seq Level 3",
+                    "Bulk WES Level 1",
+                    "Bulk WES Level 2",
+                    "CDS Sequencing Template",
+                    "Bulk WES Level 3",
+                    "Electron Microscopy Level 1",
+                    "Electron Microscopy Level 2",
+                    "Electron Microscopy Level 3",
+                    "Electron Microscopy Level 4",
+                    "ExSeq Minimal",
+                    "HI-C-seq Level 1",
+                    "HI-C-seq Level 2",
+                    "HI-C-seq Level 3",
+                    "HTAN RPPA Antibody Table",
+                    "Imaging Level 1",
+                    "Imaging Level 2",
+                    "Imaging Level 3 Segmentation",
+                    "Imaging Level 4",
+                    "Mass Spectrometry Level 1",
+                    "Mass Spectrometry Level 2",
+                    "Mass Spectrometry Level 3",
+                    "Mass Spectrometry Level 4",
+                    "Microarray Level 1",
+                    "Microarray Level 2",
+                    "Multiplexed CITE-seq Level 1",
+                    "Multiplexed CITE-seq Level 2",
+                    "Multiplexed CITE-seq Level 3",
+                    "Multiplexed CITE-seq Level 4",
+                    "Nanostring CosMx SMI Experiment",
+                    "NanoString GeoMx DSP Spatial Transcriptomics Level 1",
+                    "NanoString GeoMx DSP Spatial Transcriptomics Level 3",
+                    "Other Assay",
+                    "RPPA Level 2",
+                    "RPPA Level 3",
+                    "RPPA Level 4",
+                    "SRRS Imaging Level 2",
+                    "Slide-seq Level 1",
+                    "Slide-seq Level 2",
+                    "Slide-seq Level 3",
+                    "scATAC-seq Level 1",
+                    "scATAC-seq Level 2",
+                    "scATAC-seq Level 3",
+                    "scATAC-seq Level 4",
+                    "scDNA-seq Level 1",
+                    "scDNA-seq Level 2",
+                    "scRNA-seq Level 1",
+                    "scRNA-seq Level 2",
+                    "scRNA-seq Level 3",
+                    "scRNA-seq Level 4",
+                    "scmC-seq Level 1",
+                    "scmC-seq Level 2",
+                    "Accessory Manifest",
+                    "Acute Lymphoblastic Leukemia Tier 3",
+                    "Biospecimen",
+                    "Breast Cancer Tier 3",
+                    "Clinical Data Tier 2",
+                    "Colorectal Cancer Tier 3",
+                    "Demographics",
+                    "Diagnosis",
+                    "Exposure",
+                    "Family History",
+                    "Follow Up",
+                    "Lung Cancer Tier 3",
+                    "Melanoma Tier 3",
+                    "Molecular Test",
+                    "NanoString GeoMx DSP ROI DCC Segment Annotation Metadata",
+                    "NanoString GeoMx DSP ROI RCC Segment Annotation Metadata",
+                    "Neuroblastoma and Glioma Tier 3",
+                    "Ovarian Cancer Tier 3",
+                    "Pancreatic Cancer Tier 3",
+                    "Participant Vital Status Update",
+                    "Precancer Diagnosis",
+                    "Prostate Cancer Tier 3",
+                    "Publication Manifest",
+                    "SRRS Biospecimen",
+                    "SRRS Clinical Data Tier 2",
+                    "Sarcoma Tier 3",
+                    "Therapy",
+                ],
+            ),
         ],
         ids=[
-            "example model",
+            "example model - local",
+            "HTAN model - url",
         ],
     )
     def test_extract_components(
         self, helpers, data_model: str, expected_components: list[str]
     ):
+        # GIVEN a data model from a local file or a url
         if data_model.startswith("example"):
             data_model = helpers.get_data_path(data_model)
+
+        # WHEN an instance of the GeneratorDirector class is created
         generator = GeneratorDirector(data_model=data_model)
+        # AND the _extract_components method is called
         identified_components = generator._extract_components()
 
-        assert expected_components == identified_components
+        # THEN all components from the data model should be identified
+        assert expected_components.sort() == identified_components.sort()
 
 
 class TestJsonSchemaComponentGenerator:
