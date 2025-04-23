e441a34c0755a6cefd84849acfaa79a45dfd2695
GiaJordan <gianna.jordan@sagebase.org>
2025-04-23 11:34:26 -0700

    remove attribte def outside init

--- schematic/schemas/json_schema_component_generator.py
+++ schematic/schemas/json_schema_component_generator.py
@@ -1,7 +1,7 @@
 "JSON schema file generator"
 
 # pylint: disable=line-too-long
-# pylint: disable=attribute-defined-outside-init
+
 
 import os
 from pathlib import Path
@@ -142,10 +142,7 @@ class JsonSchemaComponentGenerator:
     ):
         self.data_model = data_model
         self.parsed_model = parsed_model
-        self._get_data_model_graph_explorer()
-
-        if component is None:
-            raise ValueError("A component must be provided")
+        self.dmge = self._get_data_model_graph_explorer()
 
         # the component can be provided as either a class label or display name
         # internally all the work is done with the class label
@@ -178,7 +175,7 @@ class JsonSchemaComponentGenerator:
         # Generate graph
         graph_data_model = data_model_grapher.graph
 
-        self.dmge = DataModelGraphExplorer(graph_data_model)
+        return DataModelGraphExplorer(graph_data_model)
 
     def get_component_json_schema(
         self,
