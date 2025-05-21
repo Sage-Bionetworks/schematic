894e255cdd9f8da1e1c9bc17b12d60a1952ef7c8
Andrew Lamb <andrewelamb@gmail.com>
2025-05-21 11:03:07 -0700

    ran black

--- schematic/schemas/data_model_parser.py
+++ schematic/schemas/data_model_parser.py
@@ -537,7 +537,9 @@ class DataModelJSONLDParser:
                         )
         return attr_rel_dictionary
 
-    def _check_allowed_values(self, entry_id: str, value: Any, relationship: str) -> None:
+    def _check_allowed_values(
+        self, entry_id: str, value: Any, relationship: str
+    ) -> None:
         """Checks that the entry is in the allowed values if they exist for the relationship
 
         Args:
