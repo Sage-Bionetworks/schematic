d0823ea90a18a2829ccba67840d6d925d30308cf
Andrew Lamb <andrewelamb@gmail.com>
2025-05-23 13:38:50 -0700

    ran pre-commit

--- tests/unit/test_json_schema_component_generator.py
+++ tests/unit/test_json_schema_component_generator.py
@@ -73,7 +73,7 @@ class TestJsonSchemaGeneratorDirector:
                     "MockComponent",
                     "MockRDB",
                     "MockFilename",
-                    "JSONSchemaComponent"
+                    "JSONSchemaComponent",
                 ],
             ),
         ],
