54742ca2d4007027ad23a37900a44ea5d3204835
Andrew Lamb <andrewelamb@gmail.com>
2025-05-25 13:59:49 -0700

    tag tests as local only

--- tests/integration/test_manifest_submission.py
+++ tests/integration/test_manifest_submission.py
@@ -28,7 +28,7 @@ def request_headers(syn_token: str) -> Dict[str, str]:
     return headers
 
 
-@pytest.mark.single_process_execution
+@pytest.mark.local_only
 class TestManifestSubmission:
     def validate_submitted_manifest_file(
         self,
