5e747effb7129951cf003a6547317f95a4645edb
GiaJordan <gianna.jordan@sagebase.org>
2025-05-28 10:58:35 -0700

    wip add tests for new functionality

--- tests/unit/test_data_model_parser.py
+++ tests/unit/test_data_model_parser.py
@@ -1,7 +1,11 @@
 """Unit tests for DataModelParser"""
 from typing import Any
 import pytest
-from schematic.schemas.data_model_parser import DataModelJSONLDParser
+from schematic.schemas.data_model_parser import (
+    DataModelParser,
+    DataModelJSONLDParser,
+    DataModelCSVParser,
+)
 
 # pylint: disable=protected-access
 
@@ -51,3 +55,85 @@ class TestDataModelJsonLdParser:
             test_type = result[attribute]["Relationships"]["ColumnType"]
             expected_type = expected_column_types[attribute]
             assert test_type == expected_type
+
+class TestDataModelCSVParser:
+    """Unit tests for DataModelCSVParser"""
+
+    def test_parse_csv_model_with_column_type(
+        self,
+    ) -> None:
+        # GIVEN a parser
+        parser = DataModelCSVParser()
+        # AND a data model that includes the column type column
+        path_to_data_model = "tests/data/example.model.column_type_component.csv"
+        # AND a dictionary of expected column types
+        expected_column_types = {
+            "String type": "string",
+            "String type caps": "string",
+            "Int type": "integer",
+            "Int type caps": "integer",
+            "Num type": "number",
+            "Num type caps": "number",
+            "String list type": "string_list",
+            "String list type caps": "string_list",
+            "Nan type": None,
+            "Missing type": None,
+            "Boolean type": "boolean",
+            "Boolean type caps": "boolean",
+        }
+
+        # WHEN the data model is parsed
+        result = parser.parse_csv_model(path_to_data_model=path_to_data_model)
+
+        for expected_attribute, expected_type in expected_column_types.items():
+            # THEN each expected attribute should be in the attribute relationship dictionary
+            assert (
+                expected_attribute in result
+            ), f"Expected attribute '{expected_attribute}' not found in attribute relationship dictionary."
+
+            # AND the attributes with no types specified should have no column type key value pair
+            if not expected_type:
+                # If the expected type is None, we expect the column type to be missing
+                assert (
+                    "ColumnType" not in result[expected_attribute]["Relationships"]
+                ), f"Expected no column type for '{expected_attribute}', but got '{result[expected_attribute]['Relationships'].get('ColumnType')}'"
+                continue
+
+            # AND the column type of each attribute should match the expected type if a column type is specified
+            assert (
+                result[expected_attribute]["Relationships"]["ColumnType"]
+                == expected_type
+            ), f"Expected column type for '{expected_attribute}' to be '{expected_type}', but got '{result[expected_attribute]['Relationships']['ColumnType']}'"
+
+    def test_parse_csv_model_without_column_type(
+        self,
+    ) -> None:
+        # GIVEN a parser
+        parser = DataModelCSVParser()
+        # AND a data model
+        path_to_data_model = "tests/data/example.model.csv"
+        # WHEN the data model is parsed
+        result = parser.parse_csv_model(path_to_data_model=path_to_data_model)
+
+        unpacked_model_df = DataModelParser.parsed_model_as_dataframe(result)
+
+        assert (
+            "ColumnType" not in unpacked_model_df.columns
+        ), "Expected no 'ColumnType' column in the unpacked model DataFrame."
+
+    def test_parse_csv_model_with_invalid_type(
+        self,
+    ) -> None:
+        """
+        Tests DataModelCSVParser.parse_csv_model with an invalid column type
+        """
+        # GIVEN a parser
+        parser = DataModelCSVParser()
+        # AND a data model with an invalid column type
+        path_to_data_model = (
+            "tests/data/example.model.column_type_component.invalid.csv"
+        )
+
+        # WHEN the data model is parsed, THEN it should raise a ValueError
+        with pytest.raises(ValueError):
+            parser.parse_csv_model(path_to_data_model=path_to_data_model)
