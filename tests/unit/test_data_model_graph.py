9e6fe7b7cdaa5fc11be917546d8cc076f023b50f
SageGJ <gianna.jordan@sagebase.org>
2025-06-20 11:33:31 -0700

    [Schematic-318] Consolidate column type example data models (#1621)
    consolidate jsonschema/columntype data models

--- tests/unit/test_data_model_graph.py
+++ tests/unit/test_data_model_graph.py
@@ -8,30 +8,10 @@ from schematic.schemas.data_model_parser import DataModelParser
 from tests.conftest import Helpers
 
 DATA_MODEL_DICT = {"example.model.csv": "CSV", "example.model.jsonld": "JSONLD"}
-
-
-@pytest.fixture(name="column_type_dmge_jsonld", scope="module")
-def fixture_column_type_dmge_jsonld() -> DataModelGraphExplorer:
-    """Returns a DataModelGraphExplorer using the example data model with columnType attributes"""
-    data_model_parser = DataModelParser(
-        path_to_data_model="tests/data/example.model_column_type.jsonld"
-    )
-    parsed_data_model = data_model_parser.parse_model()
-    data_model_grapher = DataModelGraph(parsed_data_model)
-    graph_data_model = data_model_grapher.graph
-    return DataModelGraphExplorer(graph_data_model)
-
-
-@pytest.fixture(name="column_type_dmge_csv", scope="module")
-def fixture_column_type_dmge_csv() -> DataModelGraphExplorer:
-    """Returns a DataModelGraphExplorer using the example data model with columnType attributes"""
-    data_model_parser = DataModelParser(
-        path_to_data_model="tests/data/example.model.column_type_component.csv"
-    )
-    parsed_data_model = data_model_parser.parse_model()
-    data_model_grapher = DataModelGraph(parsed_data_model)
-    graph_data_model = data_model_grapher.graph
-    return DataModelGraphExplorer(graph_data_model)
+COLUMN_TYPE_DATA_MODEL_DICT = {
+    "example.model.column_type_component.csv": "CSV",
+    "example.model.column_type_component.jsonld": "JSONLD",
+}
 
 
 class TestDataModelGraphExplorer:
@@ -115,64 +95,52 @@ class TestDataModelGraphExplorer:
     @pytest.mark.parametrize(
         "node_label, expected_type",
         [
-            ("CheckString", "string"),
-            ("CheckNum", "number"),
-            ("CheckURL", None),
-        ],
-        ids=["CheckString", "CheckNum", "CheckURL"],
-    )
-    def test_get_node_column_type_with_node_labels_jsonld(
-        self,
-        node_label: str,
-        expected_type: Optional[JSONSchemaType],
-        column_type_dmge_jsonld: DataModelGraphExplorer,
-    ) -> None:
-        """Tests for DataModelGraphExplorer.get_node_column_type using node label"""
-        assert (
-            column_type_dmge_jsonld.get_node_column_type(node_label=node_label)
-            == expected_type
-        )
-
-    @pytest.mark.parametrize(
-        "node_label, expected_type",
-        [
             ("Stringtype", "string"),
             ("Numtype", "number"),
-            ("CheckURL", None),
+            ("Missingtype", None),
         ],
-        ids=["Stringtype", "Numtype", "CheckURL"],
+        ids=["Stringtype", "Numtpye", "Missingtype"],
     )
-    def test_get_node_column_type_with_node_labels_csv(
+    @pytest.mark.parametrize(
+        "data_model",
+        list(COLUMN_TYPE_DATA_MODEL_DICT.keys()),
+        ids=list(COLUMN_TYPE_DATA_MODEL_DICT.values()),
+    )
+    def test_get_node_column_type_with_node_labels(
         self,
         node_label: str,
         expected_type: Optional[JSONSchemaType],
-        column_type_dmge_csv: DataModelGraphExplorer,
+        data_model: str,
+        helpers: Helpers,
     ) -> None:
         """Tests for DataModelGraphExplorer.get_node_column_type using node label"""
-        assert (
-            column_type_dmge_csv.get_node_column_type(node_label=node_label)
-            == expected_type
-        )
+        dmge = helpers.get_data_model_graph_explorer(path=data_model)
+        assert dmge.get_node_column_type(node_label=node_label) == expected_type
 
     @pytest.mark.parametrize(
         "node_display_name, expected_type",
         [
-            ("Check String", "string"),
-            ("Check Num", "number"),
-            ("Check URL", None),
+            ("String type", "string"),
+            ("Num type", "number"),
+            ("Missing type", None),
         ],
-        ids=["Check String", "Check Num", "Check URL"],
+        ids=["String type", "Num type", "Missing type"],
     )
-    def test_get_node_column_type_with_display_name(
+    @pytest.mark.parametrize(
+        "data_model",
+        list(COLUMN_TYPE_DATA_MODEL_DICT.keys()),
+        ids=list(COLUMN_TYPE_DATA_MODEL_DICT.values()),
+    )
+    def test_get_node_column_type_with_node_display_names(
         self,
         node_display_name: str,
         expected_type: Optional[JSONSchemaType],
-        column_type_dmge_jsonld: DataModelGraphExplorer,
+        data_model: str,
+        helpers: Helpers,
     ) -> None:
-        """Tests for DataModelGraphExplorer.get_node_column_type using node display name"""
+        """Tests for DataModelGraphExplorer.get_node_column_type using node label"""
+        dmge = helpers.get_data_model_graph_explorer(path=data_model)
         assert (
-            column_type_dmge_jsonld.get_node_column_type(
-                node_display_name=node_display_name
-            )
+            dmge.get_node_column_type(node_display_name=node_display_name)
             == expected_type
         )
